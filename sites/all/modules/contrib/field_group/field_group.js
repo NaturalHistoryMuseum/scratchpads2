dc1d6057128e8ca1cbb3518032cd2b73e341d8b2
Simon Rycroft <s.rycroft@nhm.ac.uk>
2011-02-22 13:05:09 +0000

    Added field_group 7.x-1.0-rc1

---
+++ sites/all/modules/contrib/field_group/field_group.js
@@ -0,0 +1,95 @@
+// $Id: field_group.js,v 1.5.2.12 2011/01/07 09:52:30 stalski Exp $
+
+(function($) {
+
+/**
+ * Drupal FieldGroup object.
+ */
+Drupal.FieldGroup = Drupal.FieldGroup || {};
+Drupal.FieldGroup.Effects = Drupal.FieldGroup.Effects || {};
+
+/**
+ * Implements Drupal.FieldGroup.processHook().
+ */
+Drupal.FieldGroup.Effects.processAccordion = {
+  execute: function (context, settings) {
+    $('div.field-group-accordion-wrapper', context).accordion({
+      autoHeight: false,
+      active: '.field-group-accordion-active'
+    });
+  }
+}
+
+/**
+ * Implements Drupal.FieldGroup.processHook().
+ */
+Drupal.FieldGroup.Effects.processHtabs = {
+  execute: function (context, settings) {
+  }
+}
+
+/**
+ * Implements Drupal.FieldGroup.processHook().
+ * 
+ * TODO clean this up meaning check if this is really 
+ *      necessary.
+ */
+Drupal.FieldGroup.Effects.processDiv = {
+  execute: function (context, settings) {
+
+    $('div.collapsible', context).each(function() {
+      var $wrapper = $(this);
+
+      // Turn the legend into a clickable link, but retain span.field-group-format-toggler
+      // for CSS positioning.
+      var $toggler = $('span.field-group-format-toggler:first', $wrapper);
+      var $link = $('<a class="field-group-format-title" href="#"></a>');
+      $link.prepend($toggler.contents()).appendTo($toggler);
+      
+      // .wrapInner() does not retain bound events.
+      $link.click(function () {
+        var wrapper = $wrapper.get(0);
+        // Don't animate multiple times.
+        if (!wrapper.animating) {
+          wrapper.animating = true;
+          var speed = $wrapper.hasClass('speed-fast') ? 300 : 1000;
+          if ($wrapper.hasClass('effect-none') && $wrapper.hasClass('speed-none')) {
+            $('> .field-group-format-wrapper', wrapper).toggle();
+          }
+          else if ($wrapper.hasClass('effect-blind')) {
+            $('> .field-group-format-wrapper', wrapper).toggle('blind', {}, speed);
+          }
+          else {
+            $('> .field-group-format-wrapper', wrapper).toggle(speed);
+          }
+          wrapper.animating = false;
+        }
+        return false;
+      });
+      
+    });
+  }
+};
+
+/**
+ * Behaviors.
+ */
+Drupal.behaviors.fieldGroup = {
+  attach: function (context, settings) {
+    if (settings.field_group == undefined) {
+      return;
+    }
+    $('body', context).once('fieldgroup-effects', function () {
+      // Execute all of them.
+      $.each(Drupal.FieldGroup.Effects, function (func) {
+        // We check for a wrapper function in Drupal.field_group as 
+        // alternative for dynamic string function calls.
+        if (settings.field_group[func.toLowerCase().replace("process", "")] != undefined && $.isFunction(this.execute)) {
+          this.execute(context, settings);
+        }
+      });
+    });
+  }
+};
+
+})(jQuery);
