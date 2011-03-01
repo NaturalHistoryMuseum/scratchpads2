f769f1de561e7e77522f661200e916c7b346fa90
Simon Rycroft <s.rycroft@nhm.ac.uk>
2011-03-01 12:53:48 +0000

    Working twitterscript module.

--- sites/all/modules/custom/twitterscript/js/twitterscript.js
+++ sites/all/modules/custom/twitterscript/js/twitterscript.js
@@ -1,8 +1,19 @@
-Drupal.twitterscript = new Object;
-Drupal.behaviors.twitterscript = function(context){
-  Drupal.twitterscript.init(context);
-};
-Drupal.twitterscript.init = function(context){
+// jQuery $ thingy
+(function($){
+Drupal.twitterscript = Drupal.twitterscript || {};	
+function autolink(s){
+  var hlink = /\s(ht|f)tp:\/\/([^ \,\;\:\!\)\(\"\'\<\>\f\n\r\t\v])+/g;
+  return (s.replace (hlink, function ($0,$1,$2){
+    s = $0.substring(1,$0.length); 
+    // remove trailing dots, if any
+    while (s.length>0 && s.charAt(s.length-1)=='.') 
+      s=s.substring(0,s.length-1);
+    // add hlink
+    return " " + s.link(s); 
+  }));
+}
+Drupal.behaviors.twitterscript = {
+ attach: function (context, settings){
   $('.twitterscript', context).each(function(){
     var twitterscript = this;
     $.getJSON('http://search.twitter.com/search.json?rpp=5&q=' + escape($(this).html()) + '&callback=?', function(data){
@@ -19,16 +30,7 @@ Drupal.twitterscript.init = function(context){
       $(twitterscript).replaceWith(html_to_embed);
     });
   });
+ }
 }
-
-function autolink(s){
-  var hlink = /\s(ht|f)tp:\/\/([^ \,\;\:\!\)\(\"\'\<\>\f\n\r\t\v])+/g;
-  return (s.replace (hlink, function ($0,$1,$2){
-    s = $0.substring(1,$0.length); 
-    // remove trailing dots, if any
-    while (s.length>0 && s.charAt(s.length-1)=='.') 
-      s=s.substring(0,s.length-1);
-    // add hlink
-    return " " + s.link(s); 
-  }));
-}
+// jQuery $ thingy
+})(jQuery);
