2da42874e610a9ab06c0ec841f40b2e14f50a1a2
Simon Rycroft <s.rycroft@nhm.ac.uk>
2011-03-02 12:28:12 +0000

    Added gmap 7.x-1.x-dev 2011-2-25 Added location 7.x-3.x-dev 2011-2-26

---
+++ sites/all/modules/contrib/gmap/js/clustermarker_marker.js
@@ -0,0 +1,53 @@
+
+/**
+ * @file
+ * GMap Markers
+ * Martin Pearman's ClusterMarker version
+ */
+
+/*global ClusterMarker, Drupal, GMarker */
+
+// Replace to override marker creation
+Drupal.gmap.factory.marker = function (loc, opts) {
+  return new GMarker(loc, opts);
+};
+
+Drupal.gmap.addHandler('gmap', function (elem) {
+  var obj = this;
+
+  obj.bind('init', function () {
+    obj.clustermarker = 0;
+  });
+
+  obj.bind('iconsready', function () {
+    if (!obj.clustermarker) {
+      var options = Drupal.settings.gmap_markermanager;
+      if (options.clusterMarkerIcon.length) {
+        options.clusterMarkerIcon = Drupal.gmap.getIcon(options.clusterMarkerIcon, 0);
+      }
+      else {
+        delete options.clusterMarkerIcon;
+      }
+      options.borderPadding = +options.borderPadding;
+      options.fitMapMaxZoom = +options.fitMapMaxZoom;
+      options.intersectPadding = +options.intersectPadding;
+      obj.clusterMarker = new ClusterMarker(obj.map, options);
+    }
+  });
+
+  obj.bind('addmarker', function (marker) {
+    obj.clusterMarker.addMarkers([marker.marker]);
+  });
+
+  obj.bind('delmarker', function (marker) {
+    // @@@TODO: Implement this!
+  });
+
+  obj.bind('clearmarkers', function () {
+    obj.clusterMarker.removeMarkers();
+  });
+
+  obj.bind('markersready', function () {
+    obj.clusterMarker.refresh();
+  });
+});
