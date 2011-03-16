7b379b096a18b1f046f4ee70b31362ee6b7037c6
Simon Rycroft <s.rycroft@nhm.ac.uk>
2011-03-16 16:56:34 +0000

    Cleaned up JS.  Added hover option.  Saves settings for each module/delta seperately.

--- sites/all/modules/custom/blockexpose/js/blockexpose.js
+++ sites/all/modules/custom/blockexpose/js/blockexpose.js
@@ -1,25 +1,60 @@
 (function($){
-Drupal.behaviors.blockexpose = {
- attach: function (context, settings){
-  $('.blockexpose .open-close', context).click(function(){
-    if($(this).parent().parent().hasClass('right')){
-      if($(this).parent().parent().css('right') == '-375px'){
-        $(this).parent().parent().animate({right:0}, 1000);
-        blockexpose_hidden = false;
-      } else {
-        $(this).parent().parent().animate({right:-375}, 1000);
-        blockexpose_hidden = true;      
-      }
-    }else{
-      if($(this).parent().parent().css('left') == '-375px'){
-        $(this).parent().parent().animate({left:0}, 1000);
-        blockexpose_hidden = false;
-      } else {
-        $(this).parent().parent().animate({left:-375}, 1000);
-        blockexpose_hidden = true;      
-      }      
+  Drupal.behaviors.blockexpose = {
+    attach: function(context, settings){
+      $('.blockexpose .open-close.click', context).click(function(){
+        if($(this).parent().parent().hasClass('right')) {
+          if($(this).parent().parent().css('right') == '-375px') {
+            $(this).parent().parent().animate({
+              right: 0
+            }, 1000);
+          } else {
+            $(this).parent().parent().animate({
+              right: -375
+            }, 1000);
+          }
+        } else {
+          if($(this).parent().parent().css('left') == '-375px') {
+            $(this).parent().parent().animate({
+              left: 0
+            }, 1000);
+          } else {
+            $(this).parent().parent().animate({
+              left: -375
+            }, 1000);
+          }
+        }
+      });
+      $('.blockexpose .open-close.hover', context).mouseover(function(){
+        if($(this).parent().parent().hasClass('right')) {
+          if($(this).parent().parent().css('right') == '-375px') {
+            $(this).parent().parent().animate({
+              right: 0
+            }, 200);
+          }
+        } else if($(this).hasClass('left')) {
+          if($(this).parent().parent().css('left') == '-375px') {
+            $(this).parent().parent().animate({
+              left: 0
+            }, 200);
+          }
+        }
+      });
+      $('.blockexpose .open-close.hover', context).parent().parent()
+          .mouseleave(function(){
+            if($(this).hasClass('right')) {
+              if($(this).css('right') == '0px' || $(this).css('right') == '0') {
+                $(this).animate({
+                  right: -375
+                }, 200);
+              }
+            } else if($(this).hasClass('left')) {
+              if($(this).css('left') == '0px' || $(this).css('left') == '0') {
+                $(this).animate({
+                  left: -375
+                }, 200);
+              }
+            }
+          });
     }
-  });
- }
-}
+  }
 })(jQuery);
