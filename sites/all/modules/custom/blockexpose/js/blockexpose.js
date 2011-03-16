c4f32230c5ff72a9a553c8cfa04bec709ca5ca18
Simon Rycroft <s.rycroft@nhm.ac.uk>
2011-03-16 12:57:44 +0000

    Fully working blockexpose module. Allows upto nine tabs per side of a page. Needs testing on IE and Safari.

--- sites/all/modules/custom/blockexpose/js/blockexpose.js
+++ sites/all/modules/custom/blockexpose/js/blockexpose.js
@@ -1,14 +1,23 @@
 (function($){
 Drupal.behaviors.blockexpose = {
  attach: function (context, settings){
-  var blockexpose_hidden = true;
   $('.blockexpose .open-close', context).click(function(){
-    if(blockexpose_hidden){
-      $('.blockexpose').animate({right:0}, 1000);
-      blockexpose_hidden = false;
-    } else {
-      $('.blockexpose').animate({right:-375}, 1000);
-      blockexpose_hidden = true;      
+    if($(this).parent().parent().hasClass('right')){
+      if($(this).parent().parent().css('right') == '-375px'){
+        $(this).parent().parent().animate({right:0}, 1000);
+        blockexpose_hidden = false;
+      } else {
+        $(this).parent().parent().animate({right:-375}, 1000);
+        blockexpose_hidden = true;      
+      }
+    }else{
+      if($(this).parent().parent().css('left') == '-375px'){
+        $(this).parent().parent().animate({left:0}, 1000);
+        blockexpose_hidden = false;
+      } else {
+        $(this).parent().parent().animate({left:-375}, 1000);
+        blockexpose_hidden = true;      
+      }      
     }
   });
  }
