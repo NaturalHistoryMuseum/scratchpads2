ee344a5511c8ee14eea33bd79f30246e7fc60407
Simon Rycroft <s.rycroft@nhm.ac.uk>
2011-06-13 16:35:45 +0100

    Changed to use the Extendible Web Socket server, and not PHPWebSockets.

--- sites/all/modules/custom/websockets/websockets.server.php
+++ sites/all/modules/custom/websockets/websockets.server.php
@@ -1,22 +1,34 @@
 #!/php -q
 <?php
-// We define a variable for the websockets port, as it needs to be the same on
-// all sites in a multisite setup, but can be changed if required (You may
-// already have services listening on the default port 8080).
-// Note, this is the default WebSockets port to be used by simple services.
-// More complex services that will react to posted content will create their
-// own sockets/servers
-DEFINE('WEBSOCKETS_PORT', 8080);
-include "websocket.class.php";
+require_once dirname(__FILE__) . '/websocketserver/server.php';
 
-// Extended basic WebSocket as ChatBot
-class DrupalSocket extends WebSocket{
+class DrupalUser extends WSBaseUser{
+
+  protected $maxIdle = 100;
+}
+
+class DrupalConfig extends WSBaseConfig{
+
+  public $address = '0';
+
+  // We define a variable for the websockets port, as it needs to be the same on
+  // all sites in a multisite setup, but can be changed if required (You may
+  // already have services listening on the default port 8080).
+  // Note, this is the default WebSockets port to be used by simple services.
+  // More complex services that will react to posted content will create their
+  // own sockets/servers
+  public $port = 8080;
+
+  public $userClass = 'DrupalUser';
+}
+
+class DrupalServer extends WebSocketServer{
+
+  protected $configClass = 'DrupalConfig';
 
   function process($user, $msg){
-    foreach($this->users as $user){
-      $this->send($user->socket, "Received: $msg");
-      # echo "Received: $msg";
-    }
+    $this->sentToAllBut($user, $msg);
   }
 }
-$master = new DrupalSocket(0, WEBSOCKETS_PORT);
+$server = new DrupalServer();
+$server->run();
