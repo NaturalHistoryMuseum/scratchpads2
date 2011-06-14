e045b392cc6c78b760bd2a20df27fc5e84ee073b
Simon Rycroft <s.rycroft@nhm.ac.uk>
2011-06-14 10:54:37 +0100

    Added js file and drupal.server.php (to make following the external git easier). Also tweaking the module and server file.

--- sites/all/modules/custom/websockets/websockets.server.php
+++ sites/all/modules/custom/websockets/websockets.server.php
@@ -1,33 +1,50 @@
-#!/php -q
+#!/usr/bin/php -q
 <?php
-require_once dirname(__FILE__) . '/websocketserver/server.php';
+require_once dirname(__FILE__) . '/websocketserver/drupal.server.php';
 
+/**
+ * DrupalUser class extends the standard WebSockets Base user class.
+ * 
+ * The additional information that is stored per user is the database 
+ * connection details to enable talk to the Drupal site.
+ * @author simor
+ */
 class DrupalUser extends WSBaseUser{
 
-  protected $maxIdle = 100;
+  // Allow only 10 seconds of idle time.
+  protected $maxIdle = 10;
+
+  // The database details for connecting to Drupal
+  protected $database_connection;
 }
 
+/**
+ * Here we override the standard WebSockets configuration.
+ * @author simor
+ */
 class DrupalConfig extends WSBaseConfig{
 
   public $address = '0';
 
-  // We define a variable for the websockets port, as it needs to be the same on
-  // all sites in a multisite setup, but can be changed if required (You may
-  // already have services listening on the default port 8080).
-  // Note, this is the default WebSockets port to be used by simple services.
-  // More complex services that will react to posted content will create their
-  // own sockets/servers
+  // The following variable must be changed if you would like to change the port
+  // that the server listens on.
   public $port = 8080;
 
+  // The Class of User to use
   public $userClass = 'DrupalUser';
+
+  // Disable debugging.
+  public $debug = FALSE;
 }
 
 class DrupalServer extends WebSocketServer{
 
+  // Config class (as defined above).
   protected $configClass = 'DrupalConfig';
 
+  // Here is where the hard work is done.  We need to connect to Drupal, 
+  // process the message, and then check if there are any messages to return.
   function process($user, $msg){
-    $msg = 'Yeah, whatever';
     $this->sendToAll($msg);
   }
 }
