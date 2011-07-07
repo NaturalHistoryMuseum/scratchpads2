6e4780a7067ed325a351244d37b82d4ab343783d
Simon Rycroft <s.rycroft@nhm.ac.uk>
2011-07-07 15:48:38 +0100

    Working, except for the search is a little buggy.

--- sites/all/modules/custom/tinytax/js/tinytax.js
+++ sites/all/modules/custom/tinytax/js/tinytax.js
@@ -24,6 +24,22 @@
           img_clicked.attr('src', Drupal.settings.tinytax.plus);
         }
       });
+      $('.tinytax-search-field', context).keypress(function(event){
+        var keyCode = event.keyCode ? event.keyCode : event.which ? event.which : event.charCode;
+        if (keyCode == 13) {
+          // Check to see if the aria-live thing is empty, if so, we search
+          if(!$(this).siblings('[aria-live="assertive"]').html()){
+            var vid = $(this).attr('id').replace('tinytax-search-field-','');
+            var tinytax_element = $(this).closest('form').siblings('.tinytax');
+            $.getJSON(Drupal.settings.tinytax.search_callback+"/"+vid+"/"+encodeURI($(this).val()), function(data){
+              $(tinytax_element).html(data[1]['data']);
+              Drupal.attachBehaviors($(tinytax_element));
+            });
+          }
+          return false;
+        }
+        return true;
+      });
     }
   }
 })(jQuery);
