a6c03f55a597936973e82a4f814d7ed92fd43655
Simon Rycroft <s.rycroft@nhm.ac.uk>
2011-09-13 17:37:11 +0100

    Nearly working Taxonomy editor.

--- sites/all/modules/custom/tui/js/tui.js
+++ sites/all/modules/custom/tui/js/tui.js
@@ -1,51 +1,163 @@
 (function($){
-  /*var is_control_or_apple_being_held_down = false;
+  Drupal.tui = Drupal.tui || {};
+  Drupal.tui.resize_frame = function(){
+    window.setTimeout(function(){
+      var height_offset = 150;
+      if($('#tui-tree-subcontainer ol').height() > ($(window).height() - height_offset)) {
+        $('#tui-tree-subcontainer').css('height',
+            ($(window).height() - height_offset) + 'px');
+        $('#tui-tree-subcontainer').css('overflow-y', 'scroll');
+      } else {
+        $('#tui-tree-subcontainer').css('height', 'auto');
+        $('#tui-tree-subcontainer').css('overflow-y', 'visible');
+      }
+      $('#tui-form').width($('#tui').width() - ($('#tui-tree').width() + 2));
+    }, 100);
+    window.setTimeout(function(){
+      if($('.tui-highlight').length){
+        Drupal.tui.scrollto($('.tui-highlight').first());
+      }
+    }, 200);
+  }
+  Drupal.tui.scrollto = function(element){
+    $('#tui-tree-subcontainer').animate(
+        {
+          scrollTop: $(element).position().top
+              + $('#tui-tree-subcontainer').scrollTop() - 50
+        }, 500);
+  }
   $(document).ready(function(){
-    $(document).bind('keydown', function(event){
-      if(event.keyCode == 17 || event.keyCode == 224){
-        is_control_or_apple_being_held_down = true;
-      }
-    });
-    $(document).bind('keyup', function(event){
-      if(event.keyCode == 17 || event.keyCode == 224){
-        is_control_or_apple_being_held_down = false;
-      }
-    });
-  });*/
+    // First load, check if we have one term highlighted, if so, load the form.
+    if($('.tui-highlight').length == 1) {
+      $('.tui-highlight').children('a').click();
+    }
+    if($('.tui-highlight').length) {
+      Drupal.tui.scrollto($('.tui-highlight').first());
+    }
+    Drupal.tui.resize_frame();
+  });
   Drupal.behaviors.tui = {
-    attach: function(context, settings){      
+    attach: function(context, settings){
+      // BUTTON CLICKS
+      $('#tui-tree-links img', context)
+          .click(
+              function(){
+                switch($(this).attr('id')){
+                  case 'tui-delete':
+                    $('#tui-click').unbind('click');
+                    var tids = '';
+                    if($('.tui-highlight').length) {
+                      $('.tui-highlight').each(function(){
+                        if(tids != '') {
+                          tids += ',';
+                        }
+                        tids += $(this).parent().data('tui-this-term');
+                      });
+                      $('#tui-tree-form').remove();
+                      $('#tui-tree-links').after(
+                          '<div id="tui-tree-form"></div>');
+                      var element_settings = {};
+                      element_settings.progress = {
+                        'type': 'throbber'
+                      };
+                      element_settings.url = $('#tui-delete').data('url')
+                          + tids;
+                      element_settings.event = 'click';
+                      Drupal.ajax['tui-click'] = new Drupal.ajax('tui-click',
+                          $('#tui-click'), element_settings);
+                      $('#tui-click').click();
+                    }
+                    break;
+                  case 'tui-search':
+                    $('#tui-click').unbind('click');
+                    $('#tui-tree-form').remove();
+                    $('#tui-tree-links')
+                        .after('<div id="tui-tree-form"></div>');
+                    var element_settings = {};
+                    element_settings.progress = {
+                      'type': 'throbber'
+                    };
+                    element_settings.url = $('#tui-search').data('url');
+                    element_settings.event = 'click';
+                    Drupal.ajax['tui-click'] = new Drupal.ajax('tui-click',
+                        $('#tui-click'), element_settings);
+                    $('#tui-click').click();
+                    break;
+                  case 'tui-add':
+                    break;
+                  case 'tui-link':
+                    break;
+                }
+              });
       // OPENING AND CLOSING
       $('li.tui-has-children>div>span', context).unbind('click');
-      $('li.tui-has-children>div>span', context).bind('click',function(e){
-        if($(this).parent().parent().hasClass('tui-closed') && $(this).parent().parent().hasClass('tui-never-opened')){
-            $(this).parent().parent().removeClass('tui-never-opened');
-            $(this).parent().parent().removeClass('tui-closed');
-            $(this).parent().parent().addClass('tui-load');
-            var clicked_term = $(this);
-            $.getJSON(Drupal.settings.tui.callback+"/"+$(this).parent().parent().data('tui-this-term'), function(data){
-              clicked_term.parent().parent().append(data);
-              clicked_term.parent().parent().removeClass('tui-load');
-              clicked_term.parent().parent().addClass('tui-open');
-              Drupal.attachBehaviors($('li[data-tui-child-of="'+clicked_term.parent().parent().data('tui-this-term')+'"]').parent().parent());
-            });
-        } else if (!$(this).parent().parent().hasClass('tui-load')){
-          $(this).parent().parent().toggleClass('tui-open');
-          $(this).parent().parent().toggleClass('tui-closed');
-          $(this).parent().next('ol').toggle();
-
-        }
-        e.stopPropagation();
-      });
+      $('li.tui-has-children>div>span', context).bind(
+          'click',
+          function(e){
+            if($(this).parent().parent().hasClass('tui-closed')
+                && $(this).parent().parent().hasClass('tui-never-opened')) {
+              $(this).parent().parent().removeClass('tui-never-opened');
+              $(this).parent().parent().removeClass('tui-closed');
+              $(this).parent().parent().addClass('tui-load');
+              var clicked_term = $(this);
+              $.getJSON(Drupal.settings.tui.callback + "/"
+                  + $(this).parent().parent().data('tui-this-term'), function(
+                data){
+                clicked_term.parent().parent().append(data);
+                Drupal.tui.resize_frame();
+                clicked_term.parent().parent().removeClass('tui-load');
+                clicked_term.parent().parent().addClass('tui-open');
+                Drupal.attachBehaviors($(
+                    'li[data-tui-child-of="'
+                        + clicked_term.parent().parent().data('tui-this-term')
+                        + '"]').parent().parent());
+                if(clicked_term.parent().hasClass('tui-highlight')) {
+                  $('div', clicked_term.parent().parent()).addClass(
+                      'tui-highlight');
+                }
+              });
+            } else if(!$(this).parent().parent().hasClass('tui-load')) {
+              $(this).parent().parent().toggleClass('tui-open');
+              $(this).parent().parent().toggleClass('tui-closed');
+              $(this).parent().next('ol').toggle();
+              Drupal.tui.resize_frame();
+            }
+            e.stopPropagation();
+          });
       // CLICK NAME FOR FORM
-      $('#tui-tree-container li>div').hover(function(){
+      $('#tui-tree-subcontainer li>div').hover(function(){
         $(this).children('.edit-term-link').css('display', 'inline');
       }, function(){
         $(this).children('.edit-term-link').css('display', 'none');
       });
+      $('a.edit-term-link', context).click(function(){
+        $('.tui-highlight').removeClass('tui-highlight');
+        $('.tui-form-display').removeClass('tui-form-display');
+        $(this).parent().addClass('tui-highlight tui-form-display');
+        Drupal.tui.resize_frame();
+      });
+      // RESIZABLE
+      $('#tui-tree', context).resizable(
+          {
+            handles: 'e',
+            resize: function(event, ui){
+              $('#tui-form').width(
+                  $('#tui').width() - ($('#tui-tree').width() + 2));
+            },
+            minWidth: 250
+          });
+      // RESIZE WINDOW
+      $(window).resize(function(){
+        Drupal.tui.resize_frame();
+        $('#tui-form').width($('#tui').width() - $('#tui-tree').width() - 2);
+      });
+      $('#tui-tree', context).resize(function(){
+        Drupal.tui.resize_frame();
+      });
       // CLICK TO HIGHLIGHT
-      $('#tui-tree-container li>div').click(function(){
+      $('#tui-tree-subcontainer li>div').click(function(){
         function tui_recurse_highlight(parent_tid){
-          $('li[data-tui-child-of="'+parent_tid+'"]').each(function(){
+          $('li[data-tui-child-of="' + parent_tid + '"]').each(function(){
             $(this).children('div').each(function(){
               $(this).addClass('tui-highlight');
               tui_recurse_highlight($(this).parent().data('tui-this-term'));
@@ -57,40 +169,52 @@
         tui_recurse_highlight($(this).parent().data('tui-this-term'));
       });
       // SORTING
-      $('#tui-tree-container>ol').nestedSortable({
-        cursorAt:{top:15},
-        disableNesting: 'no-nest',
-        forcePlaceholderSize: true,
-        handle: 'div',
-        helper: 'clone',
-        items: 'li',
-        opacity: .8,
-        placeholder: 'placeholder',
-        revert: 250,
-        tabSize: 25,
-        tolerance: 'pointer',
-        toleranceElement: '> div',
-        update:function(event, ui){
-          if($('li[data-tui-child-of="'+ui.item.data('tui-child-of')+'"]').length == 1){
-            $('li[data-tui-this-term="'+ui.item.data('tui-child-of')+'"]').removeClass('tui-open');
-            $('li[data-tui-this-term="'+ui.item.data('tui-child-of')+'"]').removeClass('tui-has-children');
-            $('li[data-tui-this-term="'+ui.item.data('tui-child-of')+'"]').addClass('tui-no-children');
-          }
-          if(ui.item.parent().parent().data('tui-this-term')){
-            ui.item.attr('data-tui-child-of', ui.item.parent().parent().data('tui-this-term'));
-            ui.item.data('tui-child-of', ui.item.parent().parent().data('tui-this-term'));
-          } else {
-            ui.item.removeAttr('data-tui-child-of');
-            ui.item.removeData('tui-child-of');
-          }
-          if(ui.item.parent().parent().hasClass('tui-no-children')){
-            ui.item.parent().parent().removeClass('tui-no-children');
-            ui.item.parent().parent().addClass('tui-open');
-            ui.item.parent().parent().addClass('tui-has-children');
-            Drupal.attachBehaviors(ui.item.parent().parent().parent());
-          }
-        }
-      });
+      $('#tui-tree-subcontainer>ol').nestedSortable(
+          {
+            cursorAt: {
+              top: 15
+            },
+            disableNesting: 'no-nest',
+            forcePlaceholderSize: true,
+            handle: 'div',
+            helper: 'clone',
+            items: 'li',
+            opacity: .8,
+            placeholder: 'placeholder',
+            revert: 250,
+            tabSize: 25,
+            tolerance: 'pointer',
+            toleranceElement: '> div',
+            update: function(event, ui){
+              if($('li[data-tui-child-of="' + ui.item.data('tui-child-of')
+                  + '"]').length == 1) {
+                $(
+                    'li[data-tui-this-term="' + ui.item.data('tui-child-of')
+                        + '"]').removeClass('tui-open');
+                $(
+                    'li[data-tui-this-term="' + ui.item.data('tui-child-of')
+                        + '"]').removeClass('tui-has-children');
+                $(
+                    'li[data-tui-this-term="' + ui.item.data('tui-child-of')
+                        + '"]').addClass('tui-no-children');
+              }
+              if(ui.item.parent().parent().data('tui-this-term')) {
+                ui.item.attr('data-tui-child-of', ui.item.parent().parent()
+                    .data('tui-this-term'));
+                ui.item.data('tui-child-of', ui.item.parent().parent().data(
+                    'tui-this-term'));
+              } else {
+                ui.item.removeAttr('data-tui-child-of');
+                ui.item.removeData('tui-child-of');
+              }
+              if(ui.item.parent().parent().hasClass('tui-no-children')) {
+                ui.item.parent().parent().removeClass('tui-no-children');
+                ui.item.parent().parent().addClass('tui-open');
+                ui.item.parent().parent().addClass('tui-has-children');
+                Drupal.attachBehaviors(ui.item.parent().parent().parent());
+              }
+            }
+          });
     }
   }
 })(jQuery);
