ad4e10a61064c813a356a6c1144b0833f1a22eca
Simon Rycroft <s.rycroft@nhm.ac.uk>
2011-10-12 11:47:06 +0100

    Points are working, and the GM3 class is pretty much there.

--- sites/all/modules/custom/gm3/js/gm3.js
+++ sites/all/modules/custom/gm3/js/gm3.js
@@ -1,114 +1,127 @@
 (function($){
-  Drupal.gm3 = Drupal.gm3 || {};
-  Drupal.gm3.active_button = Drupal.gm3.active_button || {};
+  Drupal.GM3 = function(map){
+    this.settings = map.settings;
+    this.id = map.id;
+    this.initialized = false;
+    this.tools = typeof (map.tools) != 'undefined' ? map.tools : new Array();
+    this.libraries = typeof (map.libraries) != 'undefined' ? map.libraries : new Object();
+    try {
+      $('#' + this.id).height(this.settings['height']);
+      $('#' + this.id).width(this.settings['width']);
+      this.default_settings();
+      // Create the map
+      this.google_map = new google.maps.Map(document.getElementById(this.id), this.settings);
+      this.initialized = true;
+    } catch(err) {
+      $('#' + this.id).html(Drupal.t('There has been an error with your map. Please contact an administrator.'));
+    }    
+    return this;
+  }
+
+  Drupal.GM3.prototype.add_toolbar_listeners = function(){
+    // Click the stuff!
+    $('.gm3-tools ul li div').click(function(){
+      $('.gm3-clicked').removeClass('gm3-clicked');
+      $(this).addClass('gm3-clicked');
+      if(gm3[$(this).data('gm3-class')] && gm3[$(this).data('gm3-class')].do_edit) {
+
+        // FIXME - Set the active class.
+
+        this.clear_listeners($(this).data('gm3-map-id'));
+        $(this).parent().addClass('gm3-clicked');
+        gm3[$(this).data('gm3-class')].do_edit($(this).data('gm3-map-id'));
+      } else {
+        // Default button clicked (or missing the class).
+        $('.gm3-clicked').removeClass('gm3-clicked');
+        $(this).parent().addClass('gm3-clicked');
+        this.google_map.setOptions({draggableCursor: 'pointer'});
+        this.clear_listeners(this.id);
+        this.add_edit_listeners(this.id);
+      }
+    });
+  }
+
+  Drupal.GM3.prototype.default_settings = function(){
+    // MapTypeID
+    this.settings['mapTypeId'] = eval(this.settings['mapTypeId']);
+    // Center
+    this.settings['center'] = new google.maps.LatLng(this.settings['center']['latitude'], this.settings['center']['longitude']);
+    // Map control
+    if(this.settings['mapTypeControlOptions'] && this.settings['mapTypeControlOptions']['mapTypeIds']) {
+      for(map_type in this.settings['mapTypeControlOptions']['mapTypeIds']) {
+        this.settings['mapTypeControlOptions']['mapTypeIds'][map_type] = eval(this.settings['mapTypeControlOptions']['mapTypeIds'][map_type]);
+      }
+    }
+    if(this.settings['mapTypeControlOptions'] && this.settings['mapTypeControlOptions']['position'] && this.settings['mapTypeControlOptions']['style']) {
+      this.settings['mapTypeControlOptions']['position'] = eval(this.settings['mapTypeControlOptions']['position']);
+      this.settings['mapTypeControlOptions']['style'] = eval(this.settings['mapTypeControlOptions']['style']);
+    }
+    // PanControlOptions
+    if(this.settings['panControlOptions'] && this.settings['panControlOptions']['position']) {
+      this.settings['panControlOptions']['position'] = eval(this.settings['panControlOptions']['position']);
+    }
+    // rotateControlOptions
+    if(this.settings['rotateControlOptions'] && this.settings['rotateControlOptions']['position']) {
+      this.settings['rotateControlOptions']['position'] = eval(this.settings['rotateControlOptions']['position']);
+    }
+    // scaleControlOptions
+    if(this.settings['scaleControlOptions'] && this.settings['scaleControlOptions']['position'] && this.settings['scaleControlOptions']['style']) {
+      this.settings['scaleControlOptions']['position'] = eval(this.settings['scaleControlOptions']['position']);
+      this.settings['scaleControlOptions']['style'] = eval(this.settings['scaleControlOptions']['style']);
+    }
+    // streetViewControlOptions
+    if(this.settings['streetViewControlOptions'] && this.settings['streetViewControlOptions']['position']) {
+      this.settings['streetViewControlOptions']['position'] = eval(this.settings['streetViewControlOptions']['position']);
+    }
+    // zoomControlOptions
+    if(this.settings['zoomControlOptions'] && this.settings['zoomControlOptions']['position'] && this.settings['zoomControlOptions']['style']) {
+      this.settings['zoomControlOptions']['position'] = eval(this.settings['zoomControlOptions']['position']);
+      this.settings['zoomControlOptions']['style'] = eval(this.settings['zoomControlOptions']['style']);
+    }
+  }
+
   Drupal.behaviors.gm3 = {attach: function(context, settings){
     for(map_id in Drupal.settings.gm3.maps) {
       if($('#' + map_id, context).length) {
-        try {
-          var map = Drupal.settings.gm3.maps[map_id];
-          $('#' + map_id, context).height(map['settings']['height']);
-          $('#' + map_id, context).width(map['settings']['width']);
-          // MapTypeID
-          map['settings']['mapTypeId'] = eval(map['settings']['mapTypeId']);
-          // Center
-          map['settings']['center'] = new google.maps.LatLng(map['settings']['center']['latitude'], map['settings']['center']['longitude']);
-          // Map control
-          if(map['settings']['mapTypeControlOptions'] && map['settings']['mapTypeControlOptions']['mapTypeIds']) {
-            for(map_type in map['settings']['mapTypeControlOptions']['mapTypeIds']) {
-              map['settings']['mapTypeControlOptions']['mapTypeIds'][map_type] = eval(map['settings']['mapTypeControlOptions']['mapTypeIds'][map_type]);
-            }
-          }
-          if(map['settings']['mapTypeControlOptions'] && map['settings']['mapTypeControlOptions']['position'] && map['settings']['mapTypeControlOptions']['style']) {
-            map['settings']['mapTypeControlOptions']['position'] = eval(map['settings']['mapTypeControlOptions']['position']);
-            map['settings']['mapTypeControlOptions']['style'] = eval(map['settings']['mapTypeControlOptions']['style']);
-          }
-          // PanControlOptions
-          if(map['settings']['panControlOptions'] && map['settings']['panControlOptions']['position']) {
-            map['settings']['panControlOptions']['position'] = eval(map['settings']['panControlOptions']['position']);
-          }
-          // rotateControlOptions
-          if(map['settings']['rotateControlOptions'] && map['settings']['rotateControlOptions']['position']) {
-            map['settings']['rotateControlOptions']['position'] = eval(map['settings']['rotateControlOptions']['position']);
-          }
-          // scaleControlOptions
-          if(map['settings']['scaleControlOptions'] && map['settings']['scaleControlOptions']['position'] && map['settings']['scaleControlOptions']['style']){
-            map['settings']['scaleControlOptions']['position'] = eval(map['settings']['scaleControlOptions']['position']);
-            map['settings']['scaleControlOptions']['style'] = eval(map['settings']['scaleControlOptions']['style']);
-          }
-          // streetViewControlOptions
-          if(map['settings']['streetViewControlOptions'] && map['settings']['streetViewControlOptions']['position']){
-            map['settings']['streetViewControlOptions']['position'] = eval(map['settings']['streetViewControlOptions']['position']);
-          }
-          // zoomControlOptions
-          if(map['settings']['zoomControlOptions'] && map['settings']['zoomControlOptions']['position'] && map['settings']['zoomControlOptions']['style']){
-            map['settings']['zoomControlOptions']['position'] = eval(map['settings']['zoomControlOptions']['position']);
-            map['settings']['zoomControlOptions']['style'] = eval(map['settings']['zoomControlOptions']['style']);
-          }          
-          // Create the map
-          map['google_map'] = new google.maps.Map(document.getElementById(map_id), map['settings']);
-          map['initialized'] = true;
-          // Click the stuff!
-          $('.gm3-tools ul li div').click(function(){
-            $('.gm3-clicked').removeClass('gm3-clicked');
-            $(this).addClass('gm3-clicked');
-            if(Drupal.gm3[$(this).data('gm3-class')] && Drupal.gm3[$(this).data('gm3-class')].do_edit){
-              
-              
-              // FIXME - Set the active class.
-              
-              
-              
-              Drupal.gm3.clear_listeners($(this).data('gm3-map-id'));
-              $(this).parent().addClass('gm3-clicked');
-              Drupal.gm3[$(this).data('gm3-class')].do_edit($(this).data('gm3-map-id'));
-            } else {
-              // Default button clicked (or missing the class).
-              $('.gm3-clicked').removeClass('gm3-clicked');
-              $(this).parent().addClass('gm3-clicked');
-              map['google_map'].setOptions({draggableCursor: 'pointer'});
-              Drupal.gm3.clear_listeners(map_id);
-              Drupal.gm3.add_edit_listeners(map_id);
-            }
-          });
-        } catch(err) {
-          $('#' + map_id, context).html(Drupal.t('There has been an error with your map. Please contact an administrator.'));
-        }
+        // Create the new GM3 map object.
+        Drupal.settings.gm3.maps[map_id] = new Drupal.GM3(Drupal.settings.gm3.maps[map_id]);
       }
     }
   }};
-  Drupal.gm3.clear_listeners = function(map_id){
+
+  Drupal.GM3.clear_listeners = function(map_id){
     google.maps.event.clearListeners(Drupal.settings.gm3.maps[map_id]['google_map'], "click");
     google.maps.event.clearListeners(Drupal.settings.gm3.maps[map_id]['google_map'], "mousemove");
     google.maps.event.clearListeners(Drupal.settings.gm3.maps[map_id]['google_map'], "rightclick");
     // Clear all listeners from this map.
-    for(i in Drupal.gm3){
-      if(Drupal.gm3[i]['clear_listeners']){
-        Drupal.gm3[i]['clear_listeners'](map_id);
+    for(i in gm3) {
+      if(gm3[i]['clear_listeners']) {
+        gm3[i]['clear_listeners'](map_id);
       }
     }
   }
-  Drupal.gm3.add_transfer_listeners = function(map_id){
+  Drupal.GM3.add_transfer_listeners = function(map_id){
     // Add transfer listeners so that polygons and other objects pass on their
     // clicks to the map.
-    for(i in Drupal.gm3){
-      if(Drupal.gm3[i]['add_transfer_listeners']){
-        Drupal.gm3[i]['add_transfer_listeners'](map_id);
+    for(i in gm3) {
+      if(gm3[i]['add_transfer_listeners']) {
+        gm3[i]['add_transfer_listeners'](map_id);
       }
     }
   }
-  Drupal.gm3.add_listeners = function(map_id){
+  Drupal.GM3.add_listeners = function(map_id){
     // Add listeners
-    for(i in Drupal.gm3){
-      if(Drupal.gm3[i]['add_listeners']){
-        Drupal.gm3[i]['add_listeners'](map_id);
+    for(i in gm3) {
+      if(gm3[i]['add_listeners']) {
+        gm3[i]['add_listeners'](map_id);
       }
     }
   }
-  Drupal.gm3.add_edit_listeners = function(map_id){
+  Drupal.GM3.add_edit_listeners = function(map_id){
     // Add Edit listeners
-    for(i in Drupal.gm3){
-      if(Drupal.gm3[i]['add_edit_listeners']){
-        Drupal.gm3[i]['add_edit_listeners'](map_id);
+    for(i in gm3) {
+      if(gm3[i]['add_edit_listeners']) {
+        gm3[i]['add_edit_listeners'](map_id);
       }
     }
   }
--- sites/all/modules/custom/gm3/js/gm3.point.js
+++ sites/all/modules/custom/gm3/js/gm3.point.js
@@ -1,34 +1,21 @@
 (function($){
-  Drupal.gm3 = Drupal.gm3 || {};
-  Drupal.gm3.point = Drupal.gm3.point || {};
-  Drupal.behaviors.gm3_point = {attach: function(context, settings){
-    for(map_id in Drupal.settings.gm3.maps) {
-      if(Drupal.settings.gm3.maps[map_id]['initialized'] && Drupal.settings.gm3.maps[map_id]['libraries']['point'] && !Drupal.settings.gm3.maps[map_id]['point']) {
-        Drupal.gm3.point.initialize(map_id);
-      }
-    }
-  }};
-  Drupal.gm3.point.initialize = function(map_id){
+  Drupal.GM3.points = function(map){
     // Point object.
-    Drupal.settings.gm3.maps[map_id]['point'] = Drupal.settings.gm3.maps[map_id]['point'] || {};
-    Drupal.settings.gm3.maps[map_id]['point']['points'] = new Array();
-    Drupal.settings.gm3.maps[map_id]['point']['markers'] = new Array();
-    Drupal.settings.gm3.maps[map_id]['point']['info_windows'] = new Array();
+    this.points = new Array();
+    this.markers = new Array();
+    this.info_windows = new Array();
+    this.google_map = map.google_map;
     // Icon
-    Drupal.settings.gm3.maps[map_id]['point']['marker_image'] = new google.maps.MarkerImage(Drupal.settings.gm3.settings.images.sprite, new google.maps.Size(16, 16), new google.maps.Point(0, 44), new google.maps.Point(8, 8));
+    this.marker_image = new google.maps.MarkerImage(Drupal.settings.gm3.settings.images.sprite, new google.maps.Size(16, 16), new google.maps.Point(0, 44), new google.maps.Point(8, 8));
     // Add points sent from server.
-    if(Drupal.settings.gm3.maps[map_id]['libraries']['point']['points']){
-      for(i in Drupal.settings.gm3.maps[map_id]['libraries']['point']['points']){
-        Drupal.gm3.point.add_marker(map_id, new google.maps.LatLng(Drupal.settings.gm3.maps[map_id]['libraries']['point']['points'][i]['lat'], Drupal.settings.gm3.maps[map_id]['libraries']['point']['points'][i]['long']), false, Drupal.settings.gm3.maps[map_id]['libraries']['point']['points'][i]['title'], Drupal.settings.gm3.maps[map_id]['libraries']['point']['points'][i]['content']);
+    if(map.libraries.point.points) {
+      for(i in map.libraries.point.points) {
+        this.add_marker(new google.maps.LatLng(map.libraries.point.points[i]['lat'], map.libraries.point.points[i]['long']), false, map.libraries.point.points[i]['title'], map.libraries.point.points[i]['content']);
       }
     }
     // Clusterer
-    Drupal.settings.gm3.maps[map_id]['point']['clusterer'] = new MarkerClusterer(Drupal.settings.gm3.maps[map_id]['google_map'], Drupal.settings.gm3.maps[map_id]['point']['points'], {
-      averageCenter: true,
-      maxZoom: 12,
-      minimumClusterSize: 5
-    });
-    Drupal.settings.gm3.maps[map_id]['point']['clusterer'].fitMapToMarkers();
+    this.clusterer = new MarkerClusterer(this.google_map, this.points, {averageCenter: true, maxZoom: 12, minimumClusterSize: 5});
+    this.clusterer.fitMapToMarkers();
     // Clicked to start.
     $('#' + map_id + "-point").click(function(){
       $('.gm3-clicked').removeClass('gm3-clicked');
@@ -42,51 +29,43 @@
       });
     });
     // Finally, process any of the registered "Finished points" functions.
-    if(Drupal.settings.gm3.maps[map_id]['libraries']['point']['finished']){
-      for(i in Drupal.settings.gm3.maps[map_id]['libraries']['point']['finished']){
+    if(Drupal.settings.gm3.maps[map_id]['libraries']['point']['finished']) {
+      for(i in Drupal.settings.gm3.maps[map_id]['libraries']['point']['finished']) {
         Drupal.settings.gm3.maps[map_id]['libraries']['point']['finished'][i](map_id);
-      }        
+      }
     }
   }
-  Drupal.gm3.point.add_marker = function(map_id, latLng, redraw, title, content){
-    redraw = typeof(redraw) != 'undefined' ? redraw : false;
-    title = typeof(title) != 'undefined' ? title : '';
-    content = typeof(content) != 'undefined' ? content : '';
-    var current_point = Drupal.settings.gm3.maps[map_id]['point']['points'].length;
-    Drupal.settings.gm3.maps[map_id]['point']['points'][current_point] = new google.maps.Marker({
-      position: latLng,
-      draggable: true,
-      title: title + " :: "+latLng.toString(),
-      icon: Drupal.settings.gm3.maps[map_id]['point']['marker_image']
-    });
-    if(content){
-      Drupal.settings.gm3.maps[map_id]['point']['markers'][latLng.toString()] = content;
-      google.maps.event.addListener(Drupal.settings.gm3.maps[map_id]['point']['points'][current_point], "click", function(event){
-        Drupal.settings.gm3.maps[map_id]['point']['info_windows'][latLng.toString()] = new InfoBubble({
-          map: this.map,
-          content: content,
-          position: latLng,
-          shadowStyle: 1,
-          padding: 0,
-          borderRadius: 4,
-          arrowSize: 10,
-          borderWidth: 1,
-          borderColor: '#2c2c2c',
-          disableAutoPan: true,
-          hideCloseButton: true,
-          arrowPosition: 30,
-          backgroundClassName: 'phoney',
-          arrowStyle: 2
-        });
+  Drupal.GM3.prototype.set_marker_image = function(marker_image){
+    this.marker_image = marker_image;
+  }
+  Drupal.GM3.points.prototype.add_marker = function(latLng, redraw, title, content){
+    redraw = typeof (redraw) != 'undefined' ? redraw : false;
+    title = typeof (title) != 'undefined' ? title : '';
+    content = typeof (content) != 'undefined' ? content : '';
+    var current_point = this.points.length;
+    this.points[current_point] = new google.maps.Marker({position: latLng, draggable: true, title: title + " :: " + latLng.toString(), icon: this.marker_image});
+    if(content) {
+      //Drupal.settings.gm3.maps[map_id]['point']['markers'][latLng.toString()] = content;
+      google.maps.event.addListener(this.points[current_point], "click", function(event){
+        Drupal.settings.gm3.maps[map_id]['point']['info_windows'][latLng.toString()] = new InfoBubble({map: this.map, content: content, position: latLng, shadowStyle: 1, padding: 0, borderRadius: 4, arrowSize: 10, borderWidth: 1, borderColor: '#2c2c2c', disableAutoPan: true, hideCloseButton: true, arrowPosition: 30, backgroundClassName: 'phoney', arrowStyle: 2});
         Drupal.settings.gm3.maps[map_id]['point']['info_windows'][latLng.toString()].open();
       });
     }
-    if(redraw){
+    if(redraw) {
       Drupal.settings.gm3.maps[map_id]['point']['clusterer'].addMarker(Drupal.settings.gm3.maps[map_id]['point']['points'][current_point], true);
       Drupal.settings.gm3.maps[map_id]['point']['clusterer'].repaint();
     }
   }
-  Drupal.gm3.point.clear_listeners = function(map_id){
+
+  Drupal.behaviors.gm3_point = {attach: function(context, settings){
+    for(map_id in Drupal.settings.gm3.maps) {
+      if(Drupal.settings.gm3.maps[map_id]['initialized'] && Drupal.settings.gm3.maps[map_id]['libraries']['point']) {
+        var points = new Drupal.GM3.points(Drupal.settings.gm3.maps[map_id]);
+      }
+    }
+  }};
+
+  /* Drupal.gm3.point.clear_listeners = function(map_id){
     // Clear all listeners from this map.
   }
   Drupal.gm3.point.add_transfer_listeners = function(map_id){
@@ -97,5 +76,5 @@
   }
   Drupal.gm3.point.add_edit_listeners = function(map_id){
     // Add Edit listeners
-  }
+  }*/
 })(jQuery);
