7acdda787f95c2cf6f584ab5308444c60f8309c0
Simon Rycroft <s.rycroft@nhm.ac.uk>
2011-11-08 00:52:40 +0000

    Reduced down.

--- sites/all/modules/custom/bigimage/js/bigimage.js
+++ sites/all/modules/custom/bigimage/js/bigimage.js
@@ -1,34 +1,19 @@
 (function($){
   Drupal.GM3.bigimage = function(map){
     this.GM3 = map;
-    // Quick hack to alter where we get the images from. ----------------------
     function CoordMapType() {}
-
     CoordMapType.prototype.tileSize = new google.maps.Size(256,256);
-    CoordMapType.prototype.maxZoom = 19;
-
-    CoordMapType.prototype.name = "Tile #s";
-    CoordMapType.prototype.alt = "Tile Coordinate Map Type";
-
+    CoordMapType.prototype.maxZoom = 19; 
+    var self = this;
     CoordMapType.prototype.getTile = function(coord, zoom, ownerDocument) {
       var div = ownerDocument.createElement('DIV');
-      console.log(coord);
-      div.innerHTML = '<img src="/bigimage/1/'+zoom+'/'+coord.x+'/'+coord.y+'"/>';
-      //div.innerHTML = coord;
+      div.innerHTML = '<img src="'+Drupal.settings.bigimage.callback+'/'+self.GM3.libraries.bigimage.fid+'/'+zoom+'/'+coord.x+'/'+coord.y+'"/>';
       div.style.width = this.tileSize.width + 'px';
       div.style.height = this.tileSize.height + 'px';
       return div;
     };
-
-
     var coordinateMapType = new CoordMapType();
-
-    // Now attach the coordinate map type to the map's registry
     this.GM3.google_map.mapTypes.set('coordinate',coordinateMapType);
-
-    // We can now set the map to use the 'coordinate' map type
     this.GM3.google_map.setMapTypeId('coordinate');
-
-    // Quick hack to alter where we get the images from. ----------------------
   }
 })(jQuery);
