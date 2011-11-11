fee266d5b524e38260b37386a9a0921f65640691
Simon Rycroft <s.rycroft@nhm.ac.uk>
2011-11-11 10:55:33 +0000

    Minor changes.

--- sites/all/modules/custom/bigimage/js/bigimage.js
+++ sites/all/modules/custom/bigimage/js/bigimage.js
@@ -17,7 +17,26 @@
     this.GM3.google_map.setMapTypeId('bigimage');
     var self = this;
     google.maps.event.addListener(this.GM3.google_map, 'idle', function(){
-      $('a','#' + self.GM3.id).parent().remove();
+      $('a', '#' + self.GM3.id).parent().remove();
     })
+
+    // Following has been added for debugging purposes.
+    /*
+    function CoordMapType(tileSize){
+      this.tileSize = tileSize;
+    }
+    CoordMapType.prototype.getTile = function(coord, zoom, ownerDocument){
+      var div = ownerDocument.createElement('DIV');
+      div.innerHTML = coord;
+      div.style.width = this.tileSize.width + 'px';
+      div.style.height = this.tileSize.height + 'px';
+      div.style.fontSize = '10';
+      div.style.borderStyle = 'solid';
+      div.style.borderWidth = '1px';
+      div.style.borderColor = '#AAAAAA';
+      return div;
+    };
+    this.GM3.google_map.overlayMapTypes.insertAt(0, new CoordMapType(new google.maps.Size(256, 256)));
+    */
   }
 })(jQuery);
