7b6e705cbd9bcf914fc099d281b7f7d9c4c09dfb
Ben Scott <b.scott@nhm.ac.uk>
2012-01-05 16:28:56 +0000

    added views sl;ideshow

---
+++ sites/all/modules/contrib/views_slideshow/theme/views-slideshow-pager-fields.tpl.php
@@ -0,0 +1,3 @@
+<div id="<?php print $widget_id; ?>" class="<?php print $classes; ?>">
+  <?php print $rendered_field_items; ?>
+</div>
---
+++ sites/all/modules/contrib/views_slideshow/theme/views-slideshow.tpl.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * @file
+ * Default views template for displaying a slideshow.
+ *
+ * - $view: The View object.
+ * - $options: Settings for the active style.
+ * - $rows: The rows output from the View.
+ * - $title: The title of this group of rows. May be empty.
+ *
+ * @ingroup views_templates
+ */
+?>
+
+<div class="skin-<?php print $skin; ?>">
+  <?php if (!empty($top_widget_rendered)): ?>
+    <div class="views-slideshow-controls-top clearfix">
+      <?php print $top_widget_rendered; ?>
+    </div>
+  <?php endif; ?>
+
+  <?php print $slideshow; ?>
+
+  <?php if (!empty($bottom_widget_rendered)): ?>
+    <div class="views-slideshow-controls-bottom clearfix">
+      <?php print $bottom_widget_rendered; ?>
+    </div>
+  <?php endif; ?>
+</div>
