cdd617b62968e3750e065089a140a0cf7c8df9db
Simon Rycroft <s.rycroft@nhm.ac.uk>
2012-01-05 13:58:06 +0000

    Fully working EOLAPI module. Just need to ensure it downloads paged content correctly.

--- sites/all/modules/custom/eolapi/js/eolapi.js
+++ sites/all/modules/custom/eolapi/js/eolapi.js
@@ -3,13 +3,21 @@
   Drupal.eolapi = Drupal.eolapi || {};
   // Attach behaviors
   Drupal.behaviors.eolapi = {attach: function(context, settings){
-    $('.eolapi-empty').each(function(){
-      var original_this = this;
-      $.ajax({data: $(this).data(), type: 'POST', url: Drupal.settings.eolapi.callback, success: function(data, textStatus, jqXHR){
-        $(original_this).html(data);
-      }, error: function(data, textStatus, jqXHR){
-        $(original_this).html('No results.');
-      }});
-    });
+    if($('.eolapi-empty').length) {
+      $('.eolapi-empty').each(function(){
+        var original_this = this;
+        $.ajax({async: false, data: $(original_this).data(), type: 'POST', url: Drupal.settings.eolapi.callback, success: function(data, textStatus, jqXHR){
+          if(data.length) {
+            $(original_this).html(data);
+          } else {
+            $(original_this).html('No results.');
+          }
+          $(original_this).removeClass('eolapi-empty');
+        }, error: function(data, textStatus, jqXHR){
+          $(original_this).html('No results.');
+          $(original_this).removeClass('eolapi-empty');
+        }});
+      })
+    }
   }};
 })(jQuery);
