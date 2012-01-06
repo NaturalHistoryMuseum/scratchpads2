5d9ed3b0a5e4d6ab929621f29dfda4c4fec91844
Simon Rycroft <s.rycroft@nhm.ac.uk>
2012-01-06 11:35:32 +0000

    Added an image field for the stillimage bundle. Tweaked the JS to use async AJAX (which it should), but to do the callbacks sequentially. Other minor changes.

--- sites/all/modules/custom/eolapi/js/eolapi.js
+++ sites/all/modules/custom/eolapi/js/eolapi.js
@@ -2,22 +2,25 @@
   // EOLAPI object.
   Drupal.eolapi = Drupal.eolapi || {};
   // Attach behaviors
-  Drupal.behaviors.eolapi = {attach: function(context, settings){
+  Drupal.eolapi.do_the_do = function(){
     if($('.eolapi-empty').length) {
-      $('.eolapi-empty').each(function(){
-        var original_this = this;
-        $.ajax({async: false, data: $(original_this).data(), type: 'POST', url: Drupal.settings.eolapi.callback, success: function(data, textStatus, jqXHR){
-          if(data.length) {
-            $(original_this).html(data);
-          } else {
-            $(original_this).html('No results.');
-          }
-          $(original_this).removeClass('eolapi-empty');
-        }, error: function(data, textStatus, jqXHR){
+      var original_this = $('.eolapi-empty').first();
+      $.ajax({data: $('.eolapi-empty').first().data(), type: 'POST', url: Drupal.settings.eolapi.callback, success: function(data, textStatus, jqXHR){
+        if(data.length) {
+          $(original_this).html(data);
+        } else {
           $(original_this).html('No results.');
-          $(original_this).removeClass('eolapi-empty');
-        }});
-      })
+        }
+        $(original_this).removeClass('eolapi-empty');
+        Drupal.eolapi.do_the_do();
+      }, error: function(data, textStatus, jqXHR){
+        $(original_this).html('No results.');
+        $(original_this).removeClass('eolapi-empty');
+        Drupal.eolapi.do_the_do();
+      }});
     }
+  }
+  Drupal.behaviors.eolapi = {attach: function(context, settings){
+    Drupal.eolapi.do_the_do();
   }};
 })(jQuery);
