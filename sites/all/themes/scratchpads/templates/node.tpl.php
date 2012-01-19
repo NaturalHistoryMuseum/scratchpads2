c6c80714b515e584f35d9f44b5d147688aff0d80
Ben Scott <b.scott@nhm.ac.uk>
2012-01-19 18:00:11 +0000

    node tpl updated for species pages

---
+++ sites/all/themes/scratchpads/templates/node.tpl.php
@@ -0,0 +1,38 @@
+<article<?php print $attributes; ?>>
+  <?php print $user_picture; ?>
+  
+  <?php if ($title): ?>
+  <header>
+    <?php print render($title_prefix); ?>
+    <?php if ($view_mode == 'species'): ?>
+    <h2<?php print $title_attributes; ?>><?php print $title ?></h2>
+    <?php elseif(!$page): ?>
+    <h2<?php print $title_attributes; ?>><a href="<?php print $node_url ?>" title="<?php print $title ?>"><?php print $title ?></a></h2>
+    <?php endif; ?>
+    
+    <?php print render($title_suffix); ?>
+  </header>
+  <?php endif; ?>
+  
+  
+  <?php if ($display_submitted): ?>
+  <footer class="submitted"><?php print $date; ?> -- <?php print $name; ?></footer>
+  <?php endif; ?>  
+  
+  <div<?php print $content_attributes; ?>>
+    <?php
+      // We hide the comments and links now so that we can render them later.
+      hide($content['comments']);
+      hide($content['links']);
+      print render($content);
+    ?>
+  </div>
+  
+  <div class="clearfix">
+    <?php if (!empty($content['links'])): ?>
+      <nav class="links node-links clearfix"><?php print render($content['links']); ?></nav>
+    <?php endif; ?>
+
+    <?php print render($content['comments']); ?>
+  </div>
+</article>
