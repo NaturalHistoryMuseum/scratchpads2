4fb57323d6f844225f129a3a9abcad6a2e743af7
Simon Rycroft <s.rycroft@nhm.ac.uk>
2012-01-24 10:12:31 +0000

    Display the file entity in a popup with simple rendering.

--- sites/all/modules/custom/scratchpads/scratchpads_file_colorboxed/js/scratchpads_file_colorboxed.js
+++ sites/all/modules/custom/scratchpads/scratchpads_file_colorboxed/js/scratchpads_file_colorboxed.js
@@ -1,5 +1,9 @@
 (function($){
   $(document).ready(function(){
-    $('a[href^="'+Drupal.settings.basePath+'file/"]').colorbox();
+    $('a[href^="'+Drupal.settings.basePath+'file/"]').colorbox({
+      rel:'gallery',
+      fastIframe:false,
+      data:'colorbox'
+    });
   });
 })(jQuery);
