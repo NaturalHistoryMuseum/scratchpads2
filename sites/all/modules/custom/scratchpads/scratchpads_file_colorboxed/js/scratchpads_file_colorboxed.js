52c05c364dca7ebd64ce5aa520f8b5834e493090
Ben Scott <b.scott@nhm.ac.uk>
2012-02-02 11:41:24 +0000

    Added behavious to colorbox js

--- sites/all/modules/custom/scratchpads/scratchpads_file_colorboxed/js/scratchpads_file_colorboxed.js
+++ sites/all/modules/custom/scratchpads/scratchpads_file_colorboxed/js/scratchpads_file_colorboxed.js
@@ -1,12 +1,17 @@
-(function($){
-  $(document).ready(function(){
-    try {
-      $('a[href^="' + Drupal.settings.basePath + 'file/"]').each(function(){
-        if(!$(this).attr('href').match('/file/[0-9]*/(.*)') && $(this).colorbox){
-          $(this).attr('href', $(this).attr('href').replace(Drupal.settings.basePath + 'file/', Drupal.settings.basePath + 'file-colorboxed/'))
-          $(this).colorbox({rel: 'gallery'});
-        }
-      });
-    } catch(err) {}
-  });
+/**
+ * Colorbox JS
+ */
+(function ($) {
+  Drupal.behaviors.ScratchpadsColorBox = {
+   attach: function(context) {     
+     try {
+       $('a[href^="' + Drupal.settings.basePath + 'file/"]', context).each(function(){
+         if(!$(this).attr('href').match('/file/[0-9]*/(.*)') && $(this).colorbox){
+           $(this).attr('href', $(this).attr('href').replace(Drupal.settings.basePath + 'file/', Drupal.settings.basePath + 'file-colorboxed/'))
+           $(this).colorbox({rel: 'gallery'});
+         }
+       });
+     } catch(err) {}
+   }
+  }
 })(jQuery);
