1659b5debadf4571491a742210ddf63317371e47
Simon Rycroft <s.rycroft@nhm.ac.uk>
2012-02-06 12:30:32 +0000

    Better fix for aggregation.

--- sites/all/modules/custom/gm3/js/gm3.getClass.js
+++ sites/all/modules/custom/gm3/js/gm3.getClass.js
@@ -1,12 +1,8 @@
 // Why doesn't Google already do this?
-setTimeout(function(){
-  console.log(google.maps);
-  for(i in google.maps) {
-    // Note, I'm assuming that functions starting with "_" shouldn't be messed
-    // with.
-    if(typeof (google.maps[i]) === 'function' && i[0] !== '_') {
-      console.log('Adding get class function to ' + i);
-      eval("google.maps." + i + ".prototype.getClass = function(){return \"" + i + "\"}");
-    }
+for(i in google.maps) {
+  // Note, I'm assuming that functions starting with "_" shouldn't be messed
+  // with.
+  if(typeof (google.maps[i]) === 'function' && i[0] !== '_') {
+    eval("google.maps." + i + ".prototype.getClass = function(){return \"" + i + "\"}");
   }
-}, 200);
+}
