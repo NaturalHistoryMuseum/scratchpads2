3aab6b4887639047edeeb98a1bf804f152f78e3e
Simon Rycroft <s.rycroft@nhm.ac.uk>
2012-02-14 12:39:31 +0000

    Working EOL/GBIF data overlay.

--- sites/all/modules/custom/eol_gbif_maps/js/eol_gbif_maps.js
+++ sites/all/modules/custom/eol_gbif_maps/js/eol_gbif_maps.js
@@ -1,1 +1,18 @@
-alert('Pish');
+(function($){
+  Drupal.GM3.eol_gbif_maps_overlay = function(map){
+    this.GM3 = map;
+    function EOLGBIFMapType(){
+      this.tileSize = new google.maps.Size(256, 256);
+    }
+    var self = this;
+    EOLGBIFMapType.prototype.getTile = function(coord, zoom, ownerDocument){
+      var div = ownerDocument.createElement('DIV');
+      console.log(Drupal.settings.gm3.settings.eol_gbif_maps.tile_url + coord.x + '_' + coord.y + '_' + zoom + '_' + self.GM3.libraries.eol_gbif_maps_overlay.taxon_id);
+      div.innerHTML = '<img class="eol_gbif_map_tile" src="' + Drupal.settings.gm3.settings.eol_gbif_maps.tile_url + coord.x + '_' + coord.y + '_' + zoom + '_' + self.GM3.libraries.eol_gbif_maps_overlay.taxon_id + '"/>';
+      div.style.width = this.tileSize.width + 'px';
+      div.style.height = this.tileSize.height + 'px';
+      return div;
+    };
+    this.GM3.google_map.overlayMapTypes.insertAt(0, new EOLGBIFMapType());
+  }
+})(jQuery);
