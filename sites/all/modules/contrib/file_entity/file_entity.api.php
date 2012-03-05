85f4fdfad0d040b673cd86bf3cd3c5a18b399e8f
Ben Scott <b.scott@nhm.ac.uk>
2012-03-05 12:16:28 +0000

    File entity updated to dev to fix VBO bug

--- sites/all/modules/contrib/file_entity/file_entity.api.php
+++ sites/all/modules/contrib/file_entity/file_entity.api.php
@@ -126,3 +126,48 @@ function hook_file_view($file, $view_mode, $langcode) {
  */
 function hook_file_view_alter($build, $type) {
 }
+
+/**
+ * Defines bulk file operations.
+ *
+ * This hook enables modules to inject custom operations into the mass
+ * operations dropdown found at admin/content/file, by associating a callback
+ * function with the operation, which is called when the form is submitted.
+ * The callback function receives one initial argument, which is an array of
+ * the checked files.
+ *
+ * @return
+ *  An associave array of operations keyed by machine name.
+ *    - label: A string to show in the operations dropdown.
+ *    - callback (string): A callback function to call for the operation. This
+ *        function will be passed an array of file_ids which were selected.
+ *    - confirm (boolean): Whether or not this operation requires a confirm form
+ *        In the case where confirm is set to true, callback should be a function
+ *        which can return a confirm form.
+ *
+ * @see hook_file_operation_info_alter()
+ * @see file_entity_get_file_operation_info()
+ */
+function hook_file_operation_info() {
+  $info['fluff'] = array(
+    'label' => t('Fluff selected files'),
+    'callback' => 'file_fluff_files',
+  );
+
+  return $info;
+}
+
+/**
+ * Perform alterations on bulk file operations.
+ *
+ * @param $info
+ *   Array of information on bulk file operations exposed by
+ *   hook_file_operation_info() implementations.
+ *
+ * @see hook_file_operation_info()
+ * @see file_entity_get_file_operation_info()
+ */
+function hook_file_operation_info_alter(&$info) {
+  // Remove the 'Fluff selected files' operation.
+  unset($info['fluff']);
+}
