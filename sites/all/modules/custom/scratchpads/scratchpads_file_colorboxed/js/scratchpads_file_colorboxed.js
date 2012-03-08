bb90ed8630c45c57fcc855cc8b6c741b6f4dc9db
Simon Rycroft <s.rycroft@nhm.ac.uk>
2012-03-08 15:11:09 +0000

    Fix JS error.

--- sites/all/modules/custom/scratchpads/scratchpads_file_colorboxed/js/scratchpads_file_colorboxed.js
+++ sites/all/modules/custom/scratchpads/scratchpads_file_colorboxed/js/scratchpads_file_colorboxed.js
@@ -5,7 +5,7 @@
   Drupal.behaviors.ScratchpadsColorBox = {attach: function(context){
     try {
       $('a[href^="' + Drupal.settings.basePath + 'file/"]', context).each(function(){
-        if(!$(this).attr('href').match('file/[0-9]*/(.*)') && !$(this).attr('href').match('file/add')$(this).colorbox) {
+        if(!$(this).attr('href').match('file/[0-9]*/(.*)') && !$(this).attr('href').match('file/add') && $(this).colorbox) {
           $(this).attr('href', $(this).attr('href').replace(Drupal.settings.basePath + 'file/', Drupal.settings.basePath + 'file-colorboxed/'))
           $(this).colorbox({rel: 'gallery'});
         }
