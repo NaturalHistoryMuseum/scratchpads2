796debdd3e5b6c920cd95ceded05e820d664407e
Simon Rycroft <s.rycroft@nhm.ac.uk>
2012-06-13 17:07:45 +0100

    Use different selectors to stop drupal_html_id from messing me up!

--- sites/all/modules/custom/itis_term/js/itis_term.js
+++ sites/all/modules/custom/itis_term/js/itis_term.js
@@ -3,26 +3,26 @@
   Drupal.itis_term = Drupal.itis_term || {};
   Drupal.itis_term.update_name_field = function(event){
     var taxon_name = '';
-    if($('#edit-field-unit-name1 input').val()) {
-      if($('#edit-field-unit-indicator1 select').val() != '_none') {
-        taxon_name += $('#edit-field-unit-indicator1 select').val() + ' ';
+    if($('[name^="field_unit_name1"]').val()) {
+      if($('[name^="field_unit_indicator1"]').val() != '_none') {
+        taxon_name += $('[name^="field_unit_indicator1"]').val() + ' ';
       }
-      taxon_name += $.trim($('#edit-field-unit-name1 input').val()) + ' ';
-      if($('#edit-field-unit-name2 input').val()) {
-        if($('#edit-field-unit-indicator2 select').val() != '_none') {
-          taxon_name += $('#edit-field-unit-indicator2 select').val() + ' ';
+      taxon_name += $.trim($('[name^="field_unit_name1"]').val()) + ' ';
+      if($('[name^="field_unit_name2"]').val()) {
+        if($('[name^="field_unit_indicator2"]').val() != '_none') {
+          taxon_name += $('[name^="field_unit_indicator2"]').val() + ' ';
         }
-        taxon_name += $.trim($('#edit-field-unit-name2 input').val()) + ' ';
+        taxon_name += $.trim($('[name^="field_unit_name2"]').val()) + ' ';
         if($('#edit-field-unit-name3 input').val()) {
-          if($('#edit-field-unit-indicator3 select').val() != '_none') {
-            taxon_name += $('#edit-field-unit-indicator3 select').val() + ' ';
+          if($('[name^="field_unit_indicator3"]').val() != '_none') {
+            taxon_name += $('[name^="field_unit_indicator3"]').val() + ' ';
           }
-          taxon_name += $.trim($('#edit-field-unit-name3 input').val()) + ' ';
-          if($('#edit-field-unit-name4 input').val()) {
-            if($('#edit-field-unit-indicator4 select').val() != '_none') {
-              taxon_name += $('#edit-field-unit-indicator4 select').val() + ' ';
+          taxon_name += $.trim($('[name^="field_unit_name3"]').val()) + ' ';
+          if($('[name^="field_unit_name4"]').val()) {
+            if($('[name^="field_unit_indicator4"]').val() != '_none') {
+              taxon_name += $('[name^="field_unit_indicator4"]').val() + ' ';
             }
-            taxon_name += $.trim($('#edit-field-unit-name4 input').val()) + ' ';
+            taxon_name += $.trim($('[name^="field_unit_indicator4"]').val()) + ' ';
           }
         }
       }
@@ -32,17 +32,17 @@
   }
   // Attach behaviours.
   Drupal.behaviors.itis_term = {attach: function(context, settings){
-    $('#edit-field-unit-name1', context).keyup(Drupal.itis_term.update_name_field);
-    $('#edit-field-unit-name2', context).keyup(Drupal.itis_term.update_name_field);
-    $('#edit-field-unit-name3', context).keyup(Drupal.itis_term.update_name_field);
-    $('#edit-field-unit-name4', context).keyup(Drupal.itis_term.update_name_field);
-    $('#edit-field-unit-name1', context).change(Drupal.itis_term.update_name_field);
-    $('#edit-field-unit-name2', context).change(Drupal.itis_term.update_name_field);
-    $('#edit-field-unit-name3', context).change(Drupal.itis_term.update_name_field);
-    $('#edit-field-unit-name4', context).change(Drupal.itis_term.update_name_field);
-    $('#edit-field-unit-indicator1', context).change(Drupal.itis_term.update_name_field);
-    $('#edit-field-unit-indicator2', context).change(Drupal.itis_term.update_name_field);
-    $('#edit-field-unit-indicator3', context).change(Drupal.itis_term.update_name_field);
-    $('#edit-field-unit-indicator4', context).change(Drupal.itis_term.update_name_field);
+    $('[name^="field_unit_name1"]', context).keyup(Drupal.itis_term.update_name_field);
+    $('[name^="field_unit_name2"]', context).keyup(Drupal.itis_term.update_name_field);
+    $('[name^="field_unit_name3"]', context).keyup(Drupal.itis_term.update_name_field);
+    $('[name^="field_unit_name4"]', context).keyup(Drupal.itis_term.update_name_field);
+    $('[name^="field_unit_name1"]', context).change(Drupal.itis_term.update_name_field);
+    $('[name^="field_unit_name2"]', context).change(Drupal.itis_term.update_name_field);
+    $('[name^="field_unit_name3"]', context).change(Drupal.itis_term.update_name_field);
+    $('[name^="field_unit_name4"]', context).change(Drupal.itis_term.update_name_field);
+    $('[name^="field_unit_indicator1"]', context).change(Drupal.itis_term.update_name_field);
+    $('[name^="field_unit_indicator2"]', context).change(Drupal.itis_term.update_name_field);
+    $('[name^="field_unit_indicator3"]', context).change(Drupal.itis_term.update_name_field);
+    $('[name^="field_unit_indicator4"]', context).change(Drupal.itis_term.update_name_field);
   }}
 })(jQuery);
