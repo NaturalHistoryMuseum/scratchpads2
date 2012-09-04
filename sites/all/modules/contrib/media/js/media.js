b3c5246c382c86d9007bdf8782ad0937483b6a4a
Simon Rycroft <s.rycroft@nhm.ac.uk>
2012-09-04 16:39:36 +0100

    Patched from http://drupal.org/node/1658452 (in patches folder as support_multiselect_in_views_library_plugins.patch).

--- sites/all/modules/contrib/media/js/media.js
+++ sites/all/modules/contrib/media/js/media.js
@@ -35,7 +35,7 @@ Drupal.behaviors.mediaElement = {
       }
 
       // When someone clicks the link to pick media (or clicks on an existing thumbnail)
-      $('.launcher', this).bind('click', function () {
+      $('.launcher', this).bind('click', function (e) {
         // Launch the browser, providing the following callback function
         // @TODO: This should not be an anomyous function.
         Drupal.media.popups.mediaBrowser(function (mediaFiles) {
@@ -49,17 +49,17 @@ Drupal.behaviors.mediaElement = {
           // Set the preview field HTML.
           previewField.html(mediaFile.preview);
         }, globalOptions);
-        return false;
+        e.preventDefault();
       });
 
       // When someone clicks the Remove button.
-      $('.remove', this).bind('click', function () {
+      $('.remove', this).bind('click', function (e) {
         // Set the value of the filefield fid (hidden) and trigger change.
         fidField.val(0);
         fidField.trigger('change');
         // Set the preview field HTML.
         previewField.html('');
-        return false;
+        e.preventDefault();
       });
 
       // Show or hide the edit/remove buttons if the field has a file or not.
