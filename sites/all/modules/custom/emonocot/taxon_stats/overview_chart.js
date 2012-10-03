fdaf994b47bec2846e546613ea5191676e8aeaa3
Edward Baker <edward.baker@nhm.ac.uk>
2012-10-03 17:45:42 +0100

    Overview taxon_stats.

---
+++ sites/all/modules/custom/emonocot/taxon_stats/overview_chart.js
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+
+      google.load("visualization", "1", {packages:["corechart"]});
+      google.setOnLoadCallback(drawChart);
+
+      function drawChart() {
+        var data = new google.visualization.DataTable();
+        data.addColumn('string', 'Type');
+        data.addColumn('number', 'Count');
+
+        jQuery('#content-by-type-data span').each(
+            function() { 
+              if (parseInt(jQuery(this).data('year')) != 0) {
+                data.addRows([ [ jQuery(this).data('type'), parseInt(jQuery(this).data('count'))/*, $tooltip*/]]);
+              }
+            } 
+          );
+        
+        
+        
+        var options = {
+            
+        };
+
+        var chart = new google.visualization.PieChart(document.getElementById('content-by-type'));
+        chart.draw(data, options);
+      }
