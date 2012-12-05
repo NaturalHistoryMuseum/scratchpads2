276f235929959ce8d26dd5827c401eb18eceef70
Simon Rycroft <s.rycroft@nhm.ac.uk>
2012-12-05 11:54:55 +0000

    Tidy and stats page. Login data also required.

---
+++ sites/all/modules/custom/scratchpads/scratchpads_statistics/js/scratchpads_statistics.js
@@ -0,0 +1,23 @@
+(function($){
+  google.load('visualization', '1.0', {'packages': ['corechart']});
+  google.setOnLoadCallback(scratchpadsStatisticsDrawChart);
+  function scratchpadsStatisticsDrawChart(){
+    $('div.google_chart').once(function(){
+      var chart_id = $(this).attr('id');
+      var chart_data = Drupal.settings.scratchpads_statistics[chart_id];
+      var data = new google.visualization.DataTable();
+      data.addColumn('string', chart_data['column_name']);
+      data.addColumn('number', chart_data['value_name']);
+      for( var key in chart_data.rows) {
+        data.addRow([key, Number(chart_data.rows[key])]);
+      }
+      var options = {'title': chart_data.title, 'width': chart_data.width, 'height': chart_data.height, 'is3D': true};
+      var chart = new google.visualization.PieChart(document.getElementById(chart_id));
+      chart.draw(data, options);
+    });
+  }
+  Drupal.behaviors.tinytax = {attach: function(context, settings){
+    // TEST this - does it work?
+    scratchpadsStatisticsDrawChart();
+  }}
+})(jQuery);
