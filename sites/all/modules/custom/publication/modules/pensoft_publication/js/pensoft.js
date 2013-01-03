ee0cd8bcb08f6a6aa6eb6cd0e74e6d9e6d2fb338
Alice Heaton <a.heaton@nhm.ac.uk>
2013-01-03 14:32:23 +0000

    Changes in the way the pensoft_publication feature works. See #103 for history.

--- sites/all/modules/custom/publication/modules/pensoft_publication/js/pensoft.js
+++ sites/all/modules/custom/publication/modules/pensoft_publication/js/pensoft.js
@@ -1,39 +1,116 @@
-/**
- * Pensoft JS tweaks
- */
- (function($){
+(function($){
 
    Drupal.behaviors.pensoft = {
 
-     attach: function(context){
-    
-      function init(){
-        
-         var element_settings = {
-           url: Drupal.settings.basePath + 'pensoft/ajax',
-           event: 'selectItem',
+     attach: function(context) {
+       
+       /**
+        * Callback when some references have been added to a textarea
+        */
+       function insert_references(inserts, settings) {
+         // Parse input and add to list of references to add
+         var $elem = $('.pensoft_add_multiple_references input');
+         var values = [];
+         var warning = [];         
+         var map = {};
+         if ($.trim($elem.val()) != "") {
+           values = $elem.val().split(",");
          }
-        
-        $('#edit-relation-select-endpoints:not(.pensoft-processed)').each(function(){        
-          $(this).bind("selectItem", selectItem).bind("deselectItem", deselectItem);
-          Drupal.ajax['pensoft'] = new Drupal.ajax('pensoft', $(this), element_settings);    
-        }).addClass('pensoft-processed');
-      
-      }
+         
+         for (var i = 0; i < inserts.length; i++) {
+           var matches = inserts[i].match(/^(\d+):(.+)$/);
+           if (matches != null && matches.length > 0) {
+             var nid = matches[1];
+             inserts[i] = matches[2];
+             
+             if (Drupal.settings["pensoft"].references_map[nid] == undefined) {
+               Drupal.settings["pensoft"].references_map[nid] = true;
+               warning.push(inserts[i]);
+             }
+             
+             values.push(nid);
+             map[inserts[i]] = nid;
+           }
+         }
+         
+         $elem.val(values);
+         
+         // Warn the user they will be available after save
+         var $warn = $('#edit-group_publication_references div.fieldset-content div.pensoft_add_multiple_references_warning');
+         if ($warn.length == 0 && warning.length > 0) {
+           $warn = $('<div class="pensoft_add_multiple_references_warning warning"><p>' +
+               Drupal.t('The following references have been added inline and will be available once the publication is saved:') +
+               '<p><ul></ul></div>').prependTo($('#edit-group_publication_references div.fieldset-content'));
+         }
+         
+         for (var i = 0; i < warning.length; i++) {
+           $('ul', $warn).append('<li class="pensoft_add_multiple_references">' + warning[i] + '</li>');
+         }
+       }
+       
+       /**
+        * Callback when some figures have been added to a textarea
+        */
+       function insert_figures(inserts, settings) {
+         // Parse input and add to list of figures to add
+         var $elem = $('.pensoft_add_multiple_figures input');
+         var values = [];
+         var warning = [];
+         var map = {};
+         if ($.trim($elem.val()) != "") {
+           values = $elem.val().split(",");
+         }
+         
+         for (var i = 0; i < inserts.length; i++) {
+           // Get fid
+           //var matches = inserts[i].match(/insert_from_view_file_(\d+)([^\d]|$)/);
+           var matches = inserts[i].match(/^(\d+):(.+)$/);
+           if (matches != null && matches.length > 0) {
+             var fid = matches[1];
+             if (Drupal.settings["pensoft"].figures_map[fid] != undefined) {
+               inserts[i] = matches[2].replace('%', Drupal.settings["pensoft"].figures_map[fid]+1);
+             } else {
+               inserts[i] = matches[2].replace('%', Drupal.settings["pensoft"].figure_key+1);
+               Drupal.settings["pensoft"].figures_map[fid] = Drupal.settings["pensoft"].figure_key;
+               Drupal.settings["pensoft"].figure_key++;
+               warning.push(inserts[i]);
+             }
+             values.push(fid);
+             map[inserts[i]] = fid;
+           }
+         }
+         
+         $elem.val(values);
+         
+         // Warn the user they will be available after save
+         var $warn = $('#edit-group_publication_figures div.fieldset-content div.pensoft_add_multiple_figures_warning');
+         if ($warn.length == 0 && warning.length > 0) {
+           $warn = $('<div class="pensoft_add_multiple_figures_warning warning"><p>' +
+               Drupal.t('The following figures have been added inline and will be available once the publication is saved:') +
+               '<p><ul></ul></div>').prependTo($('#edit-group_publication_figures div.fieldset-content'));
+         }
+         
+         for (var i = 0; i < warning.length; i++) {
+           $('ul', $warn).append('<li class="pensoft_add_multiple_figures">' + warning[i] + '</li>');
+         }
+         
+       }
     
-      function selectItem(e, id, entitydata){
-        Drupal.ajax['pensoft']['submit']['spm_entity'] = entitydata;      
+      function init() {
+        // Bind to the text area insert from view insert event
+        $('div.form-type-textarea textarea:not(.pensoft_processed)', context).bind('insertFromView-insert', function(event, inserts, settings) {
+          if (settings.name == 'Pensoft publication references') {
+            insert_references(inserts, settings);
+          } else if (settings.name == 'Pensoft publication figures') {
+            insert_figures(inserts, settings);
+          }
+        });
+
+        $('div.form-type-textarea textarea:not(.pensoft_processed)', context).addClass('pensoft_processed');
       }
-      
-      function deselectItem(e, id, entitydata){
-        $('#edit-fields').empty();     
-      } 
       
       init();
             
      }
-     
-     
-    
   };
-})(jQuery);  
+})(jQuery);
