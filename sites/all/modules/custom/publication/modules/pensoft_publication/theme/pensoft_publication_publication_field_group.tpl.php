3e5eaf792c31787d9036d0f3512c613f41c1c1fd
Alice Heaton <a.heaton@nhm.ac.uk>
2013-01-15 17:01:30 +0000

    Theme the preview of a publication to look similar to the pensoft one #2130

---
+++ sites/all/modules/custom/publication/modules/pensoft_publication/theme/pensoft_publication_publication_field_group.tpl.php
@@ -0,0 +1,89 @@
+<?php
+  /**
+   * Theme a publication group for the pensoft_publication module
+   *
+   */
+?>
+<div class="publication-group clearfix">
+  <label><?php echo $label; ?></label>
+  <?php
+    if ($element_id == 'publication_pensoft_full_group_publication_auth_contrib') :
+  ?>
+    <div class='field field-publication-background'>
+      <div class='field-items'>
+        <?php echo implode(', ', $variables['authors']); ?>
+      </div>
+    </div>
+  
+    <div class='field field-publication-background field-publication-secondary'>
+      <div class='field-items'>
+        <?php echo implode('', $variables['institutions']); ?>
+      </div>
+    </div>
+  
+    <?php  
+      $other = array('field_publication_copy_editor', 'field_publication_linguistic_editor', 'field_publication_mentor', 'field_publication_contributors');
+      foreach ($other as $field) :
+        if (!empty($element[$field]['#children'])) :
+     ?>
+          <div class='field field-publication-background field-publication-secondary'>
+            <div class='field-items'>
+              <?php echo $element[$field]['#children']; ?>
+            </div>
+          </div>
+     <?php
+        endif;
+      endforeach;
+    ?>
+  <?php elseif ($element_id == 'publication_pensoft_full_group_pensoft_files'): ?>
+    <?php /* References */ ?>
+    <div class="field">
+      <div class="field-label">
+        References:
+      </div>
+      <?php echo $element['field_publication_references'][0]['#markup']; ?>
+    </div>
+    <?php /* Figures */ ?>
+    <?php if (count(element_children($element['field_publication_figures']))) : ?>
+      <div class="field">
+        <div class="field-label">Figures:</div>
+        <div class="field-items">
+          <?php
+            $count = 0;
+            foreach (element_children($element['field_publication_figures']) as $key) :
+              $count++;
+              $fig = $element['field_publication_figures'][$key];
+          ?>
+              <div class="clearfix">
+                <?php echo str_replace('Figure', 'Figure ' . $count, $fig['#markup']); ?>
+              </div>
+          <?php endforeach; ?>          
+        </div>
+      </div>
+    <?php endif; ?>
+    <?php /* Tables */ ?>
+    <?php if (count(element_children($element['field_publication_tables']))) : ?>
+      <div class="field">
+        <div class="field-label">Tables:</div>
+        <div class="field-items">
+          <?php
+            $count = 0;
+            foreach (element_children($element['field_publication_tables']) as $key) :
+              $count++;
+          ?>
+              <div class="field field-publication-secondary">
+                <div class="field-label">
+                  <?php echo t('Table @num', array('@num' => $count)); ?>
+                </div>
+                <div class="field-items">
+                  <?php echo $element['field_publication_tables'][$key]['#markup']; ?>                
+                </div>
+              </div>
+          <? endforeach; ?>
+        </div>
+      </div>
+    <?php endif; ?>
+  <?php else: ?>
+    <?php echo $element['#children']; ?>
+  <?php endif; ?>
+</div>
