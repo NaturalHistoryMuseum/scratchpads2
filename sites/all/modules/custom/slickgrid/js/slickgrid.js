2e6f47e20b83835a69a727e0e7b3ef5504e99806
Simon Rycroft <s.rycroft@nhm.ac.uk>
2013-01-21 17:19:33 +0000

    Add Array.indexOf() function.

--- sites/all/modules/custom/slickgrid/js/slickgrid.js
+++ sites/all/modules/custom/slickgrid/js/slickgrid.js
@@ -2,6 +2,15 @@
 var grid;
 var dataView;
 
+if (!Array.prototype.indexOf) {
+  Array.prototype.indexOf = function(obj, start) {
+    for (var i = (start || 0), j = this.length; i < j; i++) {
+      if (this[i] === obj) { return i; }
+    }
+    return -1;
+  }
+}
+
 (function($) {
 
     // register namespace
