c3750750ede907ba2f5818fd7e31329928d990a4
Alice Heaton <a.heaton@nhm.ac.uk>
2013-01-22 14:36:08 +0000

    Improve the treatments tab ui

--- sites/all/modules/custom/publication/modules/pensoft_publication/js/pensoft_tabs.js
+++ sites/all/modules/custom/publication/modules/pensoft_publication/js/pensoft_tabs.js
@@ -16,6 +16,7 @@
       var selected = null;
       var last = null;
       var click_function = this.on_tab_click;
+      var tab_count = 0;
       $elements.each(function(i) {
         var elem_id = tab.get_id(this);
         var title = tab.get_title(this);
@@ -30,15 +31,21 @@
 
         var $tab_element = $('<li class="' + classname + '"><a href="' + elem_id + '">' + title + '</a></li>');
         $('a', $tab_element).click(click_function);
-        
+
         $tab_element.appendTo($tabs);
+        tab_count++;
       });
       
       if (selected == null) {
         selected = last;
       }
-      
-      $tabs.find('li.' + selected).addClass('pst_selected').find('a').click();
+     
+      if (pensoft_treatment_tab == null || tab_count != pensoft_treatment_tab_count) {
+        $tabs.find('li.' + selected).find('a').click();
+      } else {
+        $tabs.find('a[href=' + pensoft_treatment_tab + ']').click();
+      }
+      pensoft_treatment_tab_count = tab_count;
     }
     
     this.on_tab_click = function() {
@@ -46,6 +53,7 @@
       $elements.hide();
       $(this).parent().addClass('pst_selected');
       var matchc = $(this).attr('href');
+      pensoft_treatment_tab = matchc;
       $elements.filter('.' + matchc).show();
       return false;
     }
@@ -68,10 +76,6 @@
    * 
    */
   function pensoft_relation_select_setup(tabset, $root, settings) {
-    if (typeof pensoft_treatment_action[tabset] == 'undefined') {
-      pensoft_treatment_action[tabset] = 'new';
-    }
-
     var $button = $('.field-add-more-submit', $root);
     var $elements = $('div.form-type-relation-select', $root);
     
@@ -81,60 +85,50 @@
     };
 
     this.pensoft_tabs = new pensoft_tabs($root, $elements, settings);
-
+    
     /* Remove extraneous 'New treatment' tabs */    
-    //var tabs_to_remove = null;
-    // XXX should have API on the tabs class for this
-    if ((pensoft_treatment_action[tabset] != 'loaded') && this.pensoft_tabs.has_complete_tab()) {
-      //tabs_to_remove = $('div.pensoft_tabs .pst_new', $root);
-      $('div.pensoft_tabs li', $root).first().find('a').click();
+    var tabs_to_remove = null;
+    if (settings.reload && this.pensoft_tabs.has_complete_tab()) {
+      tabs_to_remove = $('div.pensoft_tabs .pst_new', $root);
     } else if ($('div.pensoft_tabs .pst_new', $root).length > 1) {
-      //tabs_to_remove = $('div.pensoft_tabs .pst_new', $root).slice(0, -1)
+      tabs_to_remove = $('div.pensoft_tabs .pst_new', $root).slice(0, -1)
     }
-    //if (tabs_to_remove != null) {
-    //  tabs_to_remove.each(function() {
-    //    var id = '.' + $('a', $(this)).attr('href');
-    //    $(this).remove();
-    //    $(id, $root).css('display', 'none'); //remove();
-    //  });
-    //}
-    pensoft_treatment_action[tabset] = 'loaded';
-    
-    // Hide "Add another item" if we have a non-filled in entry
-    
-    //if (this.pensoft_tabs.has_new_tab()) {
-      $button.hide();
-    //} else {
-    //  var $fieldset = $root.parents('fieldset');
-    //  var visible = ($fieldset.css('display') != 'none');
-    //  if (!visible) $fieldset.show();
-    //  var last = $('div.pensoft_tabs ul li', $root).last();
-    //  $button.css('left', last.position().left + last.outerWidth());            
-    //  $button.css('top', last.position().top);          
-    //  if (!visible) $fieldset.hide();
-    //}
+    if (tabs_to_remove != null) {
+      tabs_to_remove.each(function() {
+        var id = '.' + $('a', $(this)).attr('href');
+        $(this).remove();
+        $(id, $root).css('display', 'none'); //remove();
+      });
+    }
+
+    // Place the 'Add another item' button after the last tab 
+    var $fieldset = $root.parents('fieldset');
+    var visible = ($fieldset.css('display') != 'none');
+    if (!visible) $fieldset.show();
+    var last = $('div.pensoft_tabs ul li', $root).last();
+    $button.css('left', last.position().left + last.outerWidth());  
+    $button.css('right', 'auto');
+    if (!visible) $fieldset.hide();
     
     
     // Attach select/deselect events to the items
     $('div.relation-select-entities', $root).each(function() {          
       $(this).unbind("selectItem").bind("selectItem", function(e, id, entitydata) {
-        pensoft_treatment_action[tabset] = 'selected';
         $('.field-type-relation-select', $root).fadeOut();
         $('.reload-button input', $root).trigger('refreshForm');
       }).unbind("deselectItem").bind("deselectItem", function(e, id, entitydata) {
-        pensoft_treatment_action[tabset] = 'removed';
         $('.field-type-relation-select', $root).fadeOut();
         $('.reload-button input', $root).trigger('refreshForm');
       });
     });
-    
+  
     // Hide the 'search' form for filled entities, and the fields for new entities
     $elements.each(function() {
       if ($(this).find('.relation-select-entities span.title').length == 0) {
-        // XXX should be passed in as parameter ?
         $('.field-name-field-publication-treatment-name,' + 
-          '.field-name-field-publication-treat-material,' +
-          '.field-name-field-publication-treat-desc,' +
+          '.field-name-field-publication-treat-holotype,' +
+          '.field-name-field-publication-treat-type,' +
+          '.field-name-field-publication-treat-paratype,' +
           '.field-publication-treatments-entity-fields', $(this)).css('display', 'none');
       } else {
         $('div.views-exposed-form', $(this)).css('display', 'none');
@@ -147,12 +141,19 @@
      attach: function(context) {
     
       function init() { 
-        if (typeof pensoft_treatment_action == 'undefined') {
-          pensoft_treatment_action = {};
+        if (typeof pensoft_treatment_new == 'undefined') {
+          pensoft_treatment_new = true;
+          pensoft_treatment_tab = null;
+          pensoft_treatment_tab_count = 0;
+        } else {
+          pensoft_treatment_new = false;
         }
         
         // Treatments section
+        var reload = !$('#publication-treatments-container').hasClass('pensoft_processed');
         pensoft_relation_select_setup('treatments', $('#publication-treatments-container'), {
+          new_page: pensoft_treatment_new,
+          reload: reload,
           get_id: function(elem) {
             return elem.className.match(/\bform-item-field-publication-treatments-und-\d+\b/);
           },
@@ -167,45 +168,7 @@
             return title;
           }
         });
-        
-        // Authors section
-        /*
-        pensoft_relation_select_setup('authors', $('#publication-authors-container'), {
-          get_id: function(elem) {
-            return elem.className.match(/\bform-item-field-publication-authors-und-\d+\b/);            
-          },
-          get_title: function(elem) {
-            var title = $(elem).find('.relation-select-entities span.title').html();
-            if (title == null) {
-              title = 'Add Author';
-            } else {
-              title = 'Author: ' + title;
-            }
-            
-            return title;
-          }
-          
-        });
-
-        // Contributors section
-        pensoft_relation_select_setup('contributors', $('#publication-contributors-container'), {
-          get_id: function(elem) {
-            return elem.className.match(/\bform-item-field-publication-contributors-und-\d+\b/);            
-          },
-          get_title: function(elem) {
-            var title = $(elem).find('.relation-select-entities span.title').html();
-            if (title == null) {
-              title = 'Add Contributor';
-            } else {
-              title = 'Contributor: ' + title;
-            }
-            
-            return title;
-          }
-          
-        });
-        */
-
+        $('#publication-treatments-container').addClass('pensoft_processed');
       }
       
       init();
