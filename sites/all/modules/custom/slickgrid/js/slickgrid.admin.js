bef55c32a1a7c752f69e47c447b736ce2ae05bee
Simon Rycroft <s.rycroft@nhm.ac.uk>
2013-02-26 14:33:18 +0000

    Formatted. NO CODE CHANGE.

--- sites/all/modules/custom/slickgrid/js/slickgrid.admin.js
+++ sites/all/modules/custom/slickgrid/js/slickgrid.admin.js
@@ -1,43 +1,28 @@
 /**
  * JS for admin (views)
  */
- 
- (function ($) {
-
- /**
-  * Attaches the AJAX behavior to Views exposed filter forms and key View links.
-  */
- Drupal.behaviors.slickgridAdmin = {};
- 
- Drupal.behaviors.slickgridAdmin.attach = function() {
-  
-    var dependencies = {
-      filter: 'collapsibleFilter',
-      formatter: 'collapsibleFormatter',
-    }
-    
-
-    $.each(dependencies, function(select, value) { 
-
-      $('select.'+ select).change(function(){      
-      
-        if(select == 'filter'){
+(function($){
+  /**
+   * Attaches the AJAX behavior to Views exposed filter forms and key View
+   * links.
+   */
+  Drupal.behaviors.slickgridAdmin = {};
+  Drupal.behaviors.slickgridAdmin.attach = function(){
+    var dependencies = {filter: 'collapsibleFilter', formatter: 'collapsibleFormatter',}
+    $.each(dependencies, function(select, value){
+      $('select.' + select).change(function(){
+        if(select == 'filter') {
           var dependent = 'formatter';
-        }else{
+        } else {
           var dependent = 'filter';
         }
-
-        $dependentSelect = $(this).parents('tr').find('select.'+dependent);  
-           
-        if($(this).val() == value){
+        $dependentSelect = $(this).parents('tr').find('select.' + dependent);
+        if($(this).val() == value) {
           $dependentSelect.val(dependencies[dependent]);
-        }else if($dependentSelect.val() == dependencies[dependent]){
+        } else if($dependentSelect.val() == dependencies[dependent]) {
           $dependentSelect.val('');
-        }      
+        }
       });
-    
     });
-     
- };
- })(jQuery);
- 
+  };
+})(jQuery);
--- sites/all/modules/custom/slickgrid/plugins/validators/slickgrid.validators.js
+++ sites/all/modules/custom/slickgrid/plugins/validators/slickgrid.validators.js
@@ -1,19 +1,11 @@
 /**
  * Basic required validator for slickgrid editor
  */
-
-function requiredInlineValidator(value, $input) {  
-    if (value == null || value == undefined || !value.length) {
-        return {
-            valid: false,
-            msg: "This is a required field"
-        };
-    }
-
-    else {
-        return {
-            valid: true,
-            msg: null
-        };
-    }
+function requiredInlineValidator(value, $input){
+  if(value == null || value == undefined || !value.length) {
+    return {valid: false, msg: "This is a required field"};
+  }
+  else {
+    return {valid: true, msg: null};
+  }
 }
