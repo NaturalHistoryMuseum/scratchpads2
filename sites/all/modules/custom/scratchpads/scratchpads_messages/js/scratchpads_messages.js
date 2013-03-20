b1596e9666a2c859d45ad1da281be34bf4594003
Simon Rycroft <s.rycroft@nhm.ac.uk>
2013-03-20 15:33:44 +0000

    No longer body specific. Removed console message.

--- sites/all/modules/custom/scratchpads/scratchpads_messages/js/scratchpads_messages.js
+++ sites/all/modules/custom/scratchpads/scratchpads_messages/js/scratchpads_messages.js
@@ -1,6 +1,5 @@
 (function($){
   $.prototype.body_resize_toolbar = function(){
-    console.log(Drupal.toolbar.height());
-    $('body').css('paddingTop', Drupal.toolbar.height()); 
+    $(this).css('paddingTop', Drupal.toolbar.height()); 
   }
 })(jQuery);
