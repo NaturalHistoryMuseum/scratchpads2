91f3dfd76b4e827ccacef0a7947282232da3d35c
Simon Rycroft <s.rycroft@nhm.ac.uk>
2013-04-13 15:21:50 +0100

    Partially working, although the change to using $.cookie is not complete.

--- sites/all/modules/custom/cookieguard/cookieguard.js
+++ sites/all/modules/custom/cookieguard/cookieguard.js
@@ -1,21 +1,41 @@
 (function($){
+  // Override
+  var cookieHeight = 0;
   $.cookieguard.displayMessage = function(showDelay, hideDelay){
     $.cookieguard.createCSS();
-    $('#cookieGuardMsg').css('bottom', -$('#cookieGuardMsg').outerHeight());
-    $('#cookieGuardMsg').delay(showDelay).show().animate({'bottom': 0}, $.cookieguard.settings.slideSpeed);
+    cookieHeight = $('#cookieGuardMsg').outerHeight();
+    $('#cookieGuardMsg').css('top', -cookieHeight);
+    $('#cookieGuardMsg').delay(showDelay).show().animate({'top': 0}, $.cookieguard.settings.slideSpeed);
+    $('#toolbar').delay(showDelay).animate({'top': cookieHeight}, $.cookieguard.settings.slideSpeed);
+    $('body').delay(showDelay).animate({'paddingTop': cookieHeight + parseInt($('body').css('paddingTop'))}, $.cookieguard.settings.slideSpeed);
   };
+  // Override
+  $.cookieguard.hideMessage = function(hideDelay){
+    if(Drupal.overlay){
+      $('body').css({'paddingTop': parseInt($('body').css('paddingTop')) - cookieHeight});
+      $('#toolbar').css({'top': 0});
+      $('#cookieGuardMsg').remove();
+      Drupal.overlay.eventhandlerAlterDisplacedElements();
+    } else {
+      $('body').delay(hideDelay).animate({'paddingTop': parseInt($('body').css('paddingTop')) - cookieHeight}, $.cookieguard.settings.slideSpeed);
+      $('#toolbar').delay(hideDelay).animate({'top': 0}, $.cookieguard.settings.slideSpeed);
+      $('#cookieGuardMsg').delay(hideDelay).animate({'top': - cookieHeight}, $.cookieguard.settings.slideSpeed, null, function(){
+        $('#cookieGuardMsg').remove();
+      });
+    }
+  }
   $(document).ready(function(){
+    $.cookieguard();
     if(Drupal.settings.cookieguard) {
-      $.cookieguard();
       for( var i in Drupal.settings.cookieguard) {
         $.cookieguard.cookies.add(Drupal.settings.cookieguard[i]['name'], Drupal.settings.cookieguard[i]['keys'], Drupal.settings.cookieguard[i]['description'], Drupal.settings.cookieguard[i]['essential']);
       }
-      $.cookieguard.run();
-      setTimeout(function(){
-        $('#denyCookies').click(function(){
-          $.cookieguard.cookies.create($.cookieguard.settings.cookiePrefix + 'initialised', '2', 365);
-        });
-      }, $.cookieguard.settings.cookieDeleteDelay + 20);
     }
+    $.cookieguard.run();
+    setTimeout(function(){
+      $('#denyCookies').click(function(){
+        $.cookieguard.cookies.create($.cookieguard.settings.cookiePrefix + 'initialised', '2', 365);
+      });
+    }, $.cookieguard.settings.cookieDeleteDelay + 20);
   });
 })(jQuery);
