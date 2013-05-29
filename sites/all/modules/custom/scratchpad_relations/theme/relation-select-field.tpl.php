cf448a7308b6e9d002e02d36ef9787d06fe729a2
K A Bouton <k.bouton@nhm.ac.uk>
2013-05-29 16:45:23 +0100

    themeing relation
    Former-commit-id: 1eeae47698c3466a91d13d444d757364bc7dd287

---
+++ sites/all/modules/custom/scratchpad_relations/theme/relation-select-field.tpl.php
@@ -0,0 +1,7 @@
+<div class="<?php print $classes; ?>"<?php print $attributes; ?>>
+  <div class="field-items"<?php print $content_attributes; ?>>
+    <?php foreach ($items as $delta => $item): ?>
+      <div class="field-item <?php print $delta % 2 ? 'odd' : 'even'; ?>"<?php print $item_attributes[$delta]; ?>><?php print render($item); ?></div>
+    <?php endforeach; ?>
+  </div>
+</div>
