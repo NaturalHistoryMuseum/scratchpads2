ce22d31358f4adf02e378e47e1d1b0f6329a68db
Simon Rycroft <s.rycroft@nhm.ac.uk>
2013-11-13 11:02:24 +0000

    Display the fails, passes and exceptions.
    Former-commit-id: 581fc586ca769c629feec731fe9dc9dde3fa7e62

--- sites/all/modules/custom/scratchpads/scratchpads_qa/js/scratchpads_qa.js
+++ sites/all/modules/custom/scratchpads/scratchpads_qa/js/scratchpads_qa.js
@@ -1,1 +1,7 @@
-Hyphenator.run();
+(function($){
+  $(document).ready(function(){
+    $('ul.scratchpads-qa h2').click(function(){
+      $(this).siblings('.hidden-table').slideToggle(1000);
+    })
+  });
+})(jQuery);
