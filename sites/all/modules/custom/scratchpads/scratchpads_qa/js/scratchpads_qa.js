4f105095bbf249ec47505a0e1b17dd0701c6b7ff
Simon Rycroft <s.rycroft@nhm.ac.uk>
2013-11-13 12:22:10 +0000

    Make it obvious that clicking on a class expands it (or at least does something). Also expand the table and link together.
    Former-commit-id: 1645f29c53743895464da136bcc1f8aa5ca70b60

--- sites/all/modules/custom/scratchpads/scratchpads_qa/js/scratchpads_qa.js
+++ sites/all/modules/custom/scratchpads/scratchpads_qa/js/scratchpads_qa.js
@@ -2,6 +2,7 @@
   $(document).ready(function(){
     $('ul.scratchpads-qa h2').click(function(){
       $(this).siblings('.hidden-table').slideToggle(1000);
+      return false;
     })
   });
 })(jQuery);
