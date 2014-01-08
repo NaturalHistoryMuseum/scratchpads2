7bf6ac3c51f77a1b144878f296cf224077a6ecac
Alice Heaton <a.heaton@nhm.ac.uk>
2014-01-08 16:48:47 +0000

    Character editor: split JS into multiple files for readability
    Former-commit-id: 35ad162b72ca7fd23d9590f1bae573b055487148

---
+++ sites/all/modules/custom/character_editor/js/character-editor-info-popups.js
@@ -0,0 +1,52 @@
+(function($){
+
+  /**
+   * ColumnHoverUI
+   *
+   * This class is used to create and manage the column/cell hover elements
+   */
+  Drupal.ColumnHoverUI = function(slickgrid){
+    /**
+     * init
+     */
+    this.init = function(){
+      this.refresh();
+    }
+
+    /**
+     * refresh
+     *
+     */
+    this.refresh = function(){
+      var ops = {
+          cssClass: "character-editor-header",
+          fill: 'rgba(0, 0, 0, .7)',
+          cssStyles: {color: 'white', 'font-size': '10px'},
+          spikeLength: 8,
+          shrinkToFit: true,
+          offsetParent: slickgrid.getContainer(),
+          positions: ['bottom']
+      };
+      $.each(slickgrid.getColumns(), function(i, col){
+        if(col.field != "character_entity_field") {
+          $('.' + col.id).bt(col.data.char, ops);
+        }
+      });
+      ops.contentSelector = this.cellHoverCallback;
+      $('div.slick-cell:not(.l0, :empty)').bt(ops);
+    }
+
+    /**
+     * cellHoverCallback
+     */
+    this.cellHoverCallback = function(){
+      var cellText = $(this).html();
+      if(cellText.length > 2) {
+        return cellText;
+      }
+      return false;
+    }
+
+    this.init();
+  }
+})(jQuery);
