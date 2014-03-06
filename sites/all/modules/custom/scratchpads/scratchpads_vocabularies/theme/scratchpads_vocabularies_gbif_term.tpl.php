49dd4ef8bd91f58a9a48679956ee50b33e1d9ab9
Simon Rycroft <s.rycroft@nhm.ac.uk>
2014-03-06 16:24:19 +0000

    Open links in new tabs.
    Former-commit-id: 6f59e5c7b5a7204f64493acdd263f83b05779687

--- sites/all/modules/custom/scratchpads/scratchpads_vocabularies/theme/scratchpads_vocabularies_gbif_term.tpl.php
+++ sites/all/modules/custom/scratchpads/scratchpads_vocabularies/theme/scratchpads_vocabularies_gbif_term.tpl.php
@@ -1,13 +1,13 @@
 <div class="scratchpads-vocabularies-term">
-  <div class="scratchpads-vocabularies-term-label">
+	<div class="scratchpads-vocabularies-term-label">
     <?php echo check_plain($label); ?>
   </div>
   <?php if ($description || $uri || $relation || $wiki): ?>
     <div class="scratchpads-vocabularies-term-other">
-      <?php echo $description ? check_plain($description) . '<br/>' : '' ?>
-      <?php echo $uri ? t('Uri:') . ' ' . l($uri, $uri) . '<br/>' : '' ?>
-      <?php echo $relation? t('Relation:') . ' ' . l($relation, $relation) . '<br/>' : '' ?>
-      <?php echo $wiki ? t('Discussion:') . ' ' . l($wiki, $wiki) . '<br/>' : '' ?>
+      <?php echo $description ? check_plain($description) . '<br/>' : ''?>
+      <?php echo $uri ? t('Uri:') . ' ' . l($uri, $uri, array('attributes' => array('target' => '_blank'))) . '<br/>' : ''?>
+      <?php echo $relation? t('Relation:') . ' ' . l($relation, $relation, array('attributes' => array('target' => '_blank'))) . '<br/>' : ''?>
+      <?php echo $wiki ? t('Discussion:') . ' ' . l($wiki, $wiki, array('attributes' => array('target' => '_blank'))) . '<br/>' : ''?>
       </div>
   <?php endif; ?>
 </div>
