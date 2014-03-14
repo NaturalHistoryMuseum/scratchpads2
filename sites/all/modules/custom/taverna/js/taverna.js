ca532209c58b8be59db105d1d27906c30da1e581
Simon Rycroft <s.rycroft@nhm.ac.uk>
2014-03-14 11:30:24 +0000

    Working (sort of).
    Former-commit-id: 00d46896beedef4fab917924c7874b3cb9b83e1b

---
+++ sites/all/modules/custom/taverna/js/taverna.js
@@ -0,0 +1,31 @@
+(function($){
+  $(document).ready(function(){
+    pmrpc.register({publicProcedureName: "reply", procedure: function(status, results){
+      interaction_reply(status, results);
+      return "OK";
+    }});
+    pmrpc.register({publicProcedureName: "getParameterValue", procedure: function(parameterName){
+      return getParameterValue(parameterName);
+    }});
+    pmrpc.register({publicProcedureName: "getInputData", procedure: function(){
+      return inputData;
+    }});
+    pmrpc.register({publicProcedureName: "getWorkflowRunId", procedure: function(){
+      return "";
+    }});
+    pmrpc.register({publicProcedureName: "setTitle", procedure: function(title){
+      return "OK";
+    }});
+    $('.taverna-dialogue').dialog({
+      title:Drupal.settings.taverna.interaction.title,
+      modal: true,
+      width: 2*(window.innerWidth/3)
+    });
+  });
+})(jQuery);
+
+function interaction_reply(status, results){
+  var outputData = JSON.stringify(results);
+  jQuery.ajax({url: Drupal.settings.taverna.interaction.callback_url, type: "POST", async: false, data: outputData, headers: {"X-Taverna-Interaction-Reply": escape(status)}});
+  return false;
+}
