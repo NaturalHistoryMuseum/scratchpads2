a872ea593b818b023f92dcedd4ef8f13b182ce4c
Simon Rycroft <s.rycroft@nhm.ac.uk>
2014-04-28 15:52:24 +0100

    Tweak the presentation and allow toggling of the block. #3892
    Former-commit-id: 66546d28d517233ec6a3f7ebc20ba7c7002b8b1c

---
+++ sites/all/modules/custom/scratchpads/scratchpads_show_taxa_revisions/js/scratchpads_show_taxa_revisions.js
@@ -0,0 +1,7 @@
+(function($){
+  $(document).ready(function(){
+    $('.scratchpads-show-taxa-toggle').click(function(){
+      $('#scratchpads-show-taxa-table').slideToggle(300);
+    });
+  });
+})(jQuery);
