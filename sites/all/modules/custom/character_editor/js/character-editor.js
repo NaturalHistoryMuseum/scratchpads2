c70d6f49a71e6047a49924b105d9bf654046139d
Simon Rycroft <s.rycroft@nhm.ac.uk>
2014-05-14 12:08:21 +0100

    Formatted. NO CODE CHANGE.
    Former-commit-id: 43ee7f5c756894e48fb40d68c13127205e76ac46

--- sites/all/modules/custom/character_editor/js/character-editor.js
+++ sites/all/modules/custom/character_editor/js/character-editor.js
@@ -4,54 +4,50 @@
 (function($){
   /**
    * Drupal Behaviour.
-   *
+   * 
    * UI elements are created here. Note that it is assumed that there is only
    * one CharacterEditor on a page.
    */
-  Drupal.behaviors.characterEditor = {
-    attach: function(context, settings){
-      $('.slickgrid-refresh-inheritance span').click(function(){
-        // Set a cookie for the current URL for redirection purposes
-        $.cookie('character_editor_url', window.parent.location.href, {expires: 1, path: '/'});
-      });
-      var $slick = $('#slickgrid', context);
-      // Overlay width fix
-      if(typeof settings.overlay === 'undefined') {
-        $slick.parent().width($('#overlay-content').find('#content').width());
-      }
-      // Bind to slickgrid init
-      if (typeof Drupal.settings.CharacterEditorInit === 'undefined' || !Drupal.settings.CharacterEditorInit){
-        Drupal.settings.CharacterEditorInit = true;
-        $slick.bind('onSlickgridInit', function(event, slickgrid){
-          // Create the tree
-          Drupal.characterTreeUI = new Drupal.CharacterTreeUI(Drupal.settings.CharacterTreeUI.mode,
-              Drupal.settings.CharacterTreeUI.width, Drupal.settings.CharacterTreeUI.tree, context, slickgrid);
-          Drupal.settings.CharacterTreeUI.tree = {};
-          // Create the cell hover
-          Drupal.characterHoverUI = new Drupal.ColumnHoverUI(slickgrid);
-          // Create the context menu
-          Drupal.characterContextMenu = new Drupal.CharacterContextMenu(slickgrid);
-          // Create the metadata manager
-          Drupal.characterMetadataManager = new Drupal.CharacterMetadataManager(slickgrid);
-          // Create the inheritance context menu
-          Drupal.characterInheritance = new Drupal.CharacterInheritance();
-          // Alter Slickgrid ajax call to specifiy which character project this is (slickgrid typically
-          // only specifies the view/display)
-          $.ajaxSetup({
-            beforeSend: function(jqXHR, settings){
-              if (settings.url.match(/slickgrid\/get\/form\/slickgrid_settings_form/)){
-                if (typeof settings.data == 'string' && settings.data.length > 0){
-                  var project_id = encodeURIComponent(Drupal.settings.CharacterTreeUI.project);
-                  settings.data = settings.data.replace(/display_id=[^&]+(&|$)/, 'display_id=' + project_id + '$1');
-                }
-              }
-            }
-          });
-        });
-      } else {
-        Drupal.characterTreeUI.updateTree(Drupal.settings.CharacterTreeUI.tree);
-        Drupal.settings.CharacterTreeUI.tree = {};
-      }
+  Drupal.behaviors.characterEditor = {attach: function(context, settings){
+    $('.slickgrid-refresh-inheritance span').click(function(){
+      // Set a cookie for the current URL for redirection purposes
+      $.cookie('character_editor_url', window.parent.location.href, {expires: 1, path: '/'});
+    });
+    var $slick = $('#slickgrid', context);
+    // Overlay width fix
+    if(typeof settings.overlay === 'undefined') {
+      $slick.parent().width($('#overlay-content').find('#content').width());
     }
-  }
+    // Bind to slickgrid init
+    if(typeof Drupal.settings.CharacterEditorInit === 'undefined' || !Drupal.settings.CharacterEditorInit) {
+      Drupal.settings.CharacterEditorInit = true;
+      $slick.bind('onSlickgridInit', function(event, slickgrid){
+        // Create the tree
+        Drupal.characterTreeUI = new Drupal.CharacterTreeUI(Drupal.settings.CharacterTreeUI.mode, Drupal.settings.CharacterTreeUI.width, Drupal.settings.CharacterTreeUI.tree, context, slickgrid);
+        Drupal.settings.CharacterTreeUI.tree = {};
+        // Create the cell hover
+        Drupal.characterHoverUI = new Drupal.ColumnHoverUI(slickgrid);
+        // Create the context menu
+        Drupal.characterContextMenu = new Drupal.CharacterContextMenu(slickgrid);
+        // Create the metadata manager
+        Drupal.characterMetadataManager = new Drupal.CharacterMetadataManager(slickgrid);
+        // Create the inheritance context menu
+        Drupal.characterInheritance = new Drupal.CharacterInheritance();
+        // Alter Slickgrid ajax call to specifiy which character project this is
+        // (slickgrid typically
+        // only specifies the view/display)
+        $.ajaxSetup({beforeSend: function(jqXHR, settings){
+          if(settings.url.match(/slickgrid\/get\/form\/slickgrid_settings_form/)) {
+            if(typeof settings.data == 'string' && settings.data.length > 0) {
+              var project_id = encodeURIComponent(Drupal.settings.CharacterTreeUI.project);
+              settings.data = settings.data.replace(/display_id=[^&]+(&|$)/, 'display_id=' + project_id + '$1');
+            }
+          }
+        }});
+      });
+    } else {
+      Drupal.characterTreeUI.updateTree(Drupal.settings.CharacterTreeUI.tree);
+      Drupal.settings.CharacterTreeUI.tree = {};
+    }
+  }}
 })(jQuery);
