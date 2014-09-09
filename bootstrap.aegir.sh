b7647331921ae05ae417390213af90c9da8b2753
Simon Rycroft <s.rycroft@nhm.ac.uk>
2014-09-09 14:41:05 +0100

    Clear the cache and run cron after the install (ensure everything works as expected).
    Former-commit-id: 5fcbfee8325a6ceed8ec4d19cbf1bea3a416aab8

--- bootstrap.aegir.sh
+++ bootstrap.aegir.sh
@@ -9,3 +9,6 @@ drush @hostmaster hosting-import @platform_scratchpads
 drush provision-save '@scratchpads.vagrant' --context_type='site' --uri='scratchpads.vagrant' --platform='@platform_scratchpads' --server='@server_master' --db_server='@server_localhost' --profile='scratchpad_2_training' --client_name='admin'
 drush @scratchpads.vagrant provision-install
 drush @hostmaster hosting-task @platform_scratchpads verify
+# Clear the cache on the site, to ensure it's working as expected
+drush @scratchpads.vagrant cc all
+drush @scratchpads.vagrant cron
