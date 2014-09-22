1e7f960d25406ff1a46a7b97cb58d552e873f2b0
Simon Rycroft <s.rycroft@nhm.ac.uk>
2014-09-22 11:08:09 +0100

    Proper output for the matrix csv view.
    Former-commit-id: 61230dd435da01f19cd47ab08783e4292ad40dc3

--- sites/all/modules/contrib/views_matrix/includes/views-view-matrix-csv.tpl.php
+++ sites/all/modules/contrib/views_matrix/includes/views-view-matrix-csv.tpl.php
@@ -9,13 +9,16 @@
  *   - $row_classes
  *   - $matrix_attributes
  */
+// We want to send the csv as a server response so switch the content
+// type and stop further processing of the page.
+drupal_add_http_header("Content-Type", "text/csv; charset=utf-8");
 $out = fopen('php://output', 'w');
 array_unshift($rows, $header);
 foreach($rows as $row_key => $row){
   foreach($row as $column_key => $column){
     if(is_array($column)){
       $rows[$row_key][$column_key] = trim(strip_tags($column['data']));
-    } else {
+    }else{
       $rows[$row_key][$column_key] = trim(strip_tags($column));
     }
   }
@@ -23,4 +26,5 @@ foreach($rows as $row_key => $row){
 foreach($rows as $row){
   fputcsv($out, $row);
 }
-exit;
+drupal_page_footer();
+exit();
