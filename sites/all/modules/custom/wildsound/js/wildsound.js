cf337e0cb2df97a532a7a3677b9da75454c78b18
Simon Rycroft <s.rycroft@nhm.ac.uk>
2014-12-11 11:19:33 +0000

    Formatted. NO CODE CHANGE.
    Former-commit-id: 0f286244d05b2bbb94979d2a3ed43e0799d6b082

--- sites/all/modules/custom/wildsound/js/wildsound.js
+++ sites/all/modules/custom/wildsound/js/wildsound.js
@@ -1,38 +1,34 @@
-function wildsound_current_time(target_element) {
-  document.getElementById(target_element).value= window.wavesurfer_1.getCurrentTime();
+function wildsound_current_time(target_element){
+  document.getElementById(target_element).value = window.wavesurfer_1.getCurrentTime();
 }
 
-function wildsound_update_comment(comment_id, timer) {
+function wildsound_update_comment(comment_id, timer){
   request = new XMLHttpRequest();
-  
-  request.onreadystatechange = function() {
-    if (request.readyState==4 && request.status==200) {
+  request.onreadystatechange = function(){
+    if(request.readyState == 4 && request.status == 200) {
       obj = JSON.parse(request.responseText);
-    
-      if (obj.status == 'complete') {
+      if(obj.status == 'complete') {
         jQuery("article[about$=comment-" + comment_id + "]").html(obj.html);
         Drupal.attachBehaviors();
         clearTimeout(timer);
       }
     }
   };
-  
   request.timeout = 20000;
   request.open("GET", Drupal.settings.wildsound.wildsound_comment_callback_url + comment_id, true);
   request.send();
 }
 
-jQuery(window).scroll(function() {
+jQuery(window).scroll(function(){
   var fieldItem = jQuery('#waveform_1').parent('.field-item');
-  if (!checkVisible(fieldItem.parent('.field-items'))) {
-      fieldItem.css("width", "100%");
-      fieldItem.css("height", "100px", "important");
-      fieldItem.css("position", "fixed");
-      fieldItem.css("top", "64px");
-      fieldItem.css("left", "0px");
-      fieldItem.css("background-color", "white");
-      fieldItem.css("z-index", "1");
-      
+  if(!checkVisible(fieldItem.parent('.field-items'))) {
+    fieldItem.css("width", "100%");
+    fieldItem.css("height", "100px", "important");
+    fieldItem.css("position", "fixed");
+    fieldItem.css("top", "64px");
+    fieldItem.css("left", "0px");
+    fieldItem.css("background-color", "white");
+    fieldItem.css("z-index", "1");
   } else {
     fieldItem.css("width", "");
     fieldItem.css("height", "");
@@ -42,28 +38,17 @@ jQuery(window).scroll(function() {
   }
 });
 
-function checkVisible( elm, eval ) {
+function checkVisible(elm, eval){
   eval = eval || "visible";
   var vpH = jQuery(window).height(), // Viewport Height
-      st = jQuery(window).scrollTop() + 100, // Scroll Top
-      y = jQuery(elm).offset().top ,
-      elementHeight = jQuery(elm).height();
- 
-  if (eval == "visible") return ((y < (vpH + st)) && (y > (st - elementHeight)));
-  if (eval == "above") return ((y < (vpH + st)));
+  st = jQuery(window).scrollTop() + 100, // Scroll Top
+  y = jQuery(elm).offset().top, elementHeight = jQuery(elm).height();
+  if(eval == "visible") return((y < (vpH + st)) && (y > (st - elementHeight)));
+  if(eval == "above") return((y < (vpH + st)));
 }
-
-jQuery(window).load(function () {
-  for (var comment_id in Drupal.settings.wildsound.comments) {
+jQuery(window).load(function(){
+  for( var comment_id in Drupal.settings.wildsound.comments) {
     var comment = Drupal.settings.wildsound.comments[comment_id];
-    window.wavesurfer_1.addRegion({
-      id: comment.id,
-      start: comment.start,
-      end: comment.end,
-      loop: false,
-      drag: false,
-      resize: false,
-      color: comment.color
-    });
+    window.wavesurfer_1.addRegion({id: comment.id, start: comment.start, end: comment.end, loop: false, drag: false, resize: false, color: comment.color});
   }
 });
