bcedb11bc790628698a5e9b43970493c2a38036e
Simon Rycroft <s.rycroft@nhm.ac.uk>
2014-12-15 12:13:49 +0000

    Formatted. NO CODE CHANGE.
    Former-commit-id: 0c6bb397faab6325035179a9df5d751fca3eacae

--- sites/all/modules/custom/wavesurfer/js/wavesurfer_module.js
+++ sites/all/modules/custom/wavesurfer/js/wavesurfer_module.js
@@ -1,46 +1,39 @@
-jQuery(document).ready(function () {
-	window.surfers = new Array();
-	jQuery('.wavesurfer-container').each(function () {
-		var index = jQuery(this).data("fileid");
-		window.surfers[index] = Object.create(WaveSurfer);
-        window.surfers[index].init({
-                container: this,
-                waveColor: jQuery(this).data("wavecolor"),
-                progressColor: jQuery(this).data("progresscolor"),
-                scrollParent: true,
-                minPxPerSec: jQuery(this).data("pixelrate"),
-                fileid: index
-        });
-        window.surfers[index].on('ready', function () {
-        	for (var index2 in Drupal.settings.wavesurfer.onready) {
-        		var onarray_f = Drupal.settings.wavesurfer.onready[index2];
-                eval(onarray_f+'(window.surfers['+index+'])'); 
-            }
-        	var progressDiv = document.querySelector('#progress-bar-'+index);
-        	progressDiv.style.display = 'none';
-            window.surfers[index].seekTo(0);
-        });
-        
-        window.surfers[index].on('loading', function (percent, xhr) {
-        	var progressDiv = document.querySelector('#progress-bar-'+index);
-        	var progressBar = progressDiv.querySelector('.progress-bar-'+index);
-        	progressDiv.style.display = 'block';
-    	    progressBar.style.width = percent + '%';
-        });
-        
-        window.surfers[index].on('destroy', function () {
-        	var progressDiv = document.querySelector('#progress-bar-'+index);
-        	progressDiv.style.display = 'block';
-        });
-        window.surfers[index].on('error', function () {
-        	var progressDiv = document.querySelector('#progress-bar-'+index);
-        	progressDiv.style.display = 'block';
-        });
+jQuery(document).ready(function(){
+  window.surfers = new Array();
+  jQuery('.wavesurfer-container').each(function(){
+    var index = jQuery(this).data("fileid");
+    window.surfers[index] = Object.create(WaveSurfer);
+    window.surfers[index].init({container: this, waveColor: jQuery(this).data("wavecolor"), progressColor: jQuery(this).data("progresscolor"), scrollParent: true, minPxPerSec: jQuery(this).data("pixelrate"), fileid: index});
+    window.surfers[index].on('ready', function(){
+      for( var index2 in Drupal.settings.wavesurfer.onready) {
+        var onarray_f = Drupal.settings.wavesurfer.onready[index2];
+        eval(onarray_f + '(window.surfers[' + index + '])');
+      }
+      var progressDiv = document.querySelector('#progress-bar-' + index);
+      progressDiv.style.display = 'none';
+      window.surfers[index].seekTo(0);
+    });
 
-        window.surfers[index].loadBuffer(jQuery(this).data("audiofile"));       
-	});
+    window.surfers[index].on('loading', function(percent, xhr){
+      var progressDiv = document.querySelector('#progress-bar-' + index);
+      var progressBar = progressDiv.querySelector('.progress-bar-' + index);
+      progressDiv.style.display = 'block';
+      progressBar.style.width = percent + '%';
+    });
+
+    window.surfers[index].on('destroy', function(){
+      var progressDiv = document.querySelector('#progress-bar-' + index);
+      progressDiv.style.display = 'block';
+    });
+    window.surfers[index].on('error', function(){
+      var progressDiv = document.querySelector('#progress-bar-' + index);
+      progressDiv.style.display = 'block';
+    });
+
+    window.surfers[index].loadBuffer(jQuery(this).data("audiofile"));
+  });
 });
 
-function wavesurfer_playsurfer(index) {
-       window.surfers[index].playPause();
+function wavesurfer_playsurfer(index){
+  window.surfers[index].playPause();
 }
