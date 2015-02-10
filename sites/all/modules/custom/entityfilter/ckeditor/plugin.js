d134bbfbe82a2b34ccf5cc87a9e3fbed7c81a62e
Simon Rycroft <s.rycroft@nhm.ac.uk>
2015-02-10 12:37:12 +0000

    Display the options over the editor.
    Former-commit-id: 010846792ddd9deb8e967c911f1c4ad66e40c571

--- sites/all/modules/custom/entityfilter/ckeditor/plugin.js
+++ sites/all/modules/custom/entityfilter/ckeditor/plugin.js
@@ -29,7 +29,6 @@ if(document.all && !window.setInterval.isPolyfill) {
  */
 function CKEDITOR_entityfilter(editor){
   this.editor = editor;
-  this.observe = 0;
   this.timeout_id = null;
   if(CKEDITOR_entityfilter.caller !== CKEDITOR_entityfilter.get_instance) {
     throw new Error("This object cannot be instanciated");
@@ -87,11 +86,24 @@ CKEDITOR_entityfilter.prototype.timeout_callback = function(args){
   var editor = entityfilter.editor;
   var range = selection.getRanges()[0];
   var text_left_of_cursor = range.startContainer.$.textContent.substring(0, range.endOffset)
+  console.log(text_left_of_cursor);
   if(text_left_of_cursor.indexOf('[entity') > -1) {
     var search_str = text_left_of_cursor.substring(text_left_of_cursor.lastIndexOf('['));
     // Ensure there are no spaces in there, else we've probably already handled
     // this brace (or it's not even an entity brace
     if(search_str.indexOf(' ') == -1) {
+      var x = 0;
+      var y = 0;
+      var obj = editor.document.getActive().$;
+      while(obj.offsetParent) {
+        x += obj.offsetLeft;
+        y += obj.offsetTop;
+        obj = obj.offsetParent;
+      }
+      x += obj.offsetLeft;
+      y += obj.offsetTop;
+      console.log(x);
+      console.log(y);
       var startOffset = parseInt(range.startOffset - search_str.length) || 0;
       var element = range.startContainer.$;
       jQuery.get(Drupal.settings.basePath + 'ckeditor/entityfilter', {
