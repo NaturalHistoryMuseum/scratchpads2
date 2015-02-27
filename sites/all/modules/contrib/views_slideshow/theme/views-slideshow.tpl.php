1b6acdcdeafa2f97568e9eedba66423ae9e3a232
Simon Rycroft <s.rycroft@nhm.ac.uk>
2015-02-27 11:20:38 +0000

    Upgraded views_slideshow to 7.x-3.1
    Former-commit-id: 998096898b2c2599ff05660c8f2ef6605ed38f2e

--- sites/all/modules/contrib/views_slideshow/theme/views-slideshow.tpl.php
+++ sites/all/modules/contrib/views_slideshow/theme/views-slideshow.tpl.php
@@ -13,18 +13,20 @@
  */
 ?>
 
-<div class="skin-<?php print $skin; ?>">
-  <?php if (!empty($top_widget_rendered)): ?>
-    <div class="views-slideshow-controls-top clearfix">
-      <?php print $top_widget_rendered; ?>
-    </div>
-  <?php endif; ?>
+<?php if (!empty($slideshow)): ?>
+  <div class="skin-<?php print $skin; ?>">
+    <?php if (!empty($top_widget_rendered)): ?>
+      <div class="views-slideshow-controls-top clearfix">
+        <?php print $top_widget_rendered; ?>
+      </div>
+    <?php endif; ?>
 
-  <?php print $slideshow; ?>
+    <?php print $slideshow; ?>
 
-  <?php if (!empty($bottom_widget_rendered)): ?>
-    <div class="views-slideshow-controls-bottom clearfix">
-      <?php print $bottom_widget_rendered; ?>
-    </div>
-  <?php endif; ?>
-</div>
+    <?php if (!empty($bottom_widget_rendered)): ?>
+      <div class="views-slideshow-controls-bottom clearfix">
+        <?php print $bottom_widget_rendered; ?>
+      </div>
+    <?php endif; ?>
+  </div>
+<?php endif; ?>
