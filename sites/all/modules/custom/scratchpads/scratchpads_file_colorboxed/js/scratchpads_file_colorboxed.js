7077b4c6fd140542e38fcabc9fd073462ef50a47
Simon Rycroft <s.rycroft@nhm.ac.uk>
2015-04-08 12:43:41 +0100

    Include the pathPrefix in the URL when checking for matches for 'C?q=file/{file ID}' URLs. Possible fix for #4365.
    Former-commit-id: a6b1e7f448fe92455503e70d7e64b97dad405fec

--- sites/all/modules/custom/scratchpads/scratchpads_file_colorboxed/js/scratchpads_file_colorboxed.js
+++ sites/all/modules/custom/scratchpads/scratchpads_file_colorboxed/js/scratchpads_file_colorboxed.js
@@ -8,7 +8,7 @@
       try {
         $('a[href^="' + Drupal.settings.basePath + Drupal.settings.pathPrefix + 'file/"]', context).each(function(){
           if(!$(this).attr('href').match('file/[0-9]*/(.*)') && !$(this).attr('href').match('file/add') && $(this).colorbox) {
-            $(this).attr('href', $(this).attr('href').replace(Drupal.settings.basePath + 'file/', Drupal.settings.basePath + 'file-colorboxed/'))
+            $(this).attr('href', $(this).attr('href').replace(Drupal.settings.basePath + Drupal.settings.pathPrefix + 'file/', Drupal.settings.basePath + Drupal.settings.pathPrefix + 'file-colorboxed/'))
             $(this).colorbox($.extend({rel: 'gallery'}, Drupal.settings.colorbox));
           }
         });
