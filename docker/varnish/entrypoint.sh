1ff613eb8df9456e75ac0d437e87ca4383750eb6
benscott <ben@benscott.co.uk>
2018-08-26 12:16:41 +0100

    Add secret

--- docker/varnish/entrypoint.sh
+++ docker/varnish/entrypoint.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+init_varnish_secret() {
+    echo -e "${VARNISH_SECRET}" > /etc/varnish/secret
+}
+
+init_varnish_secret
+
 # Start varnish and log
-varnishd -f /etc/varnish/default.vcl -s malloc,${VARNISH_MEMORY} -a 0.0.0.0:${VARNISH_PORT}
+varnishd -f /etc/varnish/default.vcl -s malloc,${VARNISH_MEMORY} -a 0.0.0.0:${VARNISH_PORT} -T 0.0.0.0:6082 -S /etc/varnish/secret
 varnishlog
