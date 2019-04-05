06ca8867f7427b48b443d650a5404657986833dd
Alice Butcher <alycejenni@gmail.com>
2019-04-05 12:34:11 +0100

    update antibot module to 7.x-1.2

--- sites/all/modules/contrib/antibot/js/antibot.js
+++ sites/all/modules/contrib/antibot/js/antibot.js
@@ -5,46 +5,55 @@
  */
 (function ($) {
   Drupal.antibot = {};
-  
+
   Drupal.behaviors.antibot = {
     attach: function (context) {
-      // Assume the user is not human, despite JS being enabled
+      // Assume the user is not human, despite JS being enabled.
       Drupal.settings.antibot.human = false;
-      
-      // Display the hidden forms
-      $('.antibot-hidden', context).show();
-      // Remove the "no javascript" messages
-      $('.antibot-no-js', context).remove();
-      
-      // Wait for a mouse to move, indicating they are human
+
+      // Wait for a mouse to move, indicating they are human.
       $('body').mousemove(function() {
-        // Unlock the forms
+        // Unlock the forms.
         Drupal.antibot.unlockForms();
       });
-      
-      // A tab or enter key pressed can also indicate they are human
+
+      // Wait for a touch move event, indicating that they are human.
+      $('body').bind('touchmove', function() {
+        // Unlock the forms.
+        Drupal.antibot.unlockForms();
+      });
+
+      // A tab or enter key pressed can also indicate they are human.
       $('body').keydown(function(e) {
         if ((e.keyCode == 9) || (e.keyCode == 13)) {
-          // Unlock the forms
+          // Unlock the forms.
           Drupal.antibot.unlockForms();
         }
       });
     }
-  }
-  
+  };
+
   /**
    * Revert the action on the protected forms to what it was originally
    * set to.
    */
   Drupal.antibot.unlockForms = function() {
-    // Act only if we haven't yet verified this user as being human
+    // Act only if we haven't yet verified this user as being human.
     if (!Drupal.settings.antibot.human) {
-      // Iterate all antibot form actions that we need to revert
-      for (n in Drupal.settings.antibot.actions) {
-        $('form#' + n).attr('action', Drupal.settings.antibot.actions[n]);
+      // Iterate all antibot forms that we need to unlock.
+      for (var id in Drupal.settings.antibot.forms) {
+        // Switch the action to the original value.
+        $('form#' + id).attr('action', Drupal.settings.antibot.forms[id].action);
+        
+        // Check if a key is required.
+        if (Drupal.settings.antibot.forms[id].key) {
+          // Inject the key value.
+          $('form#' + id).find('input[name="antibot_key"]').val(Drupal.settings.antibot.forms[id].key);
+        }
       }
-      // Mark this user as being human
+      // Mark this user as being human.
       Drupal.settings.antibot.human = true;
     }
-  }
+  };
+
 })(jQuery);
