bb692ac0cc963e25b89b3c51ec94741d9f370c48
Alice Butcher <alycejenni@gmail.com>
2019-12-19 16:35:38 +0000

    Remove container-inline class
    Doesn't work with new layout

--- sites/all/modules/custom/scratchpads/scratchpads_search_block/scratchpads-search-block-form.tpl.php
+++ sites/all/modules/custom/scratchpads/scratchpads_search_block/scratchpads-search-block-form.tpl.php
@@ -29,7 +29,7 @@
  * @see template_preprocess_search_block_form()
  */
 ?>
-<div class="container-inline">
+<div>
   <?php if (empty($variables['form']['#block']->subject)): ?>
     <h2 class="element-invisible"><?php print t('Search form'); ?></h2>
   <?php endif; ?>
