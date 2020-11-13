623ad00bfe1ac73fb17eeeeeb456cfc41ec58cdc
Paul Kiddle <p.kiddle@nhm.ac.uk>
2020-11-13 11:03:39 +0000

    Apply patches

--- misc/tableheader.js
+++ misc/tableheader.js
@@ -38,9 +38,16 @@ Drupal.tableHeader = function (table) {
 
   // Clone the table header so it inherits original jQuery properties. Hide
   // the table to avoid a flash of the header clone upon page load.
-  this.stickyTable = $('<table class="sticky-header"/>')
+  this.stickyTable = $(document.createElement('table'))
     .insertBefore(this.originalTable)
     .css({ position: 'fixed', top: '0px' });
+
+  // Copy classes from originalTable, remove undesired classes, and add sticky-header.
+  // Any other classes added to originalTable by modules will exists in stickyTable to ensure consistent styling.
+  this.stickyTable.attr('class', this.originalTable.attr('class'));
+  this.stickyTable.removeClass('sticky-enabled tableheader-processed sticky-table');
+  this.stickyTable.addClass('sticky-header');
+
   this.stickyHeader = this.originalHeader.clone(true)
     .hide()
     .appendTo(this.stickyTable);
