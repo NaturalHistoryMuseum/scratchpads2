290b300b191af75bd4030fd8c1c7336e9290ed27
Paul Kiddle <p.kiddle@nhm.ac.uk>
2020-11-23 11:58:52 +0000

    Tidy up RPC with code comments

--- sites/all/modules/custom/scratchpads/scratchpads_tweaks/RpcRouter.php
+++ sites/all/modules/custom/scratchpads/scratchpads_tweaks/RpcRouter.php
@@ -1,10 +1,26 @@
 <?php
 
+/**
+ * Provides functions that can be executed via the RPC interface (see scratchpads_tweaks_rpc function)
+ * External programs can use a JSONRPC client to call these methods transparently, as if they were defined in that program's own codebase.
+ * This makes development easier, since there's a predictable 1-to-1 mapping of the way the functions are defined and the way they're called.
+ * The inputs and outputs are designed so that consumers of the API need no knowledge of Drupal or other system internals.
+ *
+ * NB: Drupal database IDs are currently used to identify objects. Should look at alternatives.
+ */
 class RpcRouter {
 	function test($value){
 		return ['You said', $value];
 	}
 
+	/**
+	 * Get the site settings
+	 *
+	 * Returns:
+	 * 	- name => The site name
+	 *	- theme_color => The site's default colour
+	 * @return object
+	 */
 	function getSiteSettings(){
 		$colour = variable_get('scratchpads_colour', SCRATCHPADS_DEFAULT_COLOUR);
 		if($colour == 'custom'){
@@ -17,10 +33,15 @@ class RpcRouter {
 		];
 	}
 
-	function getNode($n) {
-		return node_load($n);
-	}
-
+	/**
+	 * Load an author by biblio author id
+	 *
+	 * Returns:
+	 * 	- firstName => The first name of the author (including any prefixes)
+	 * 	- lastName => The surname of the author
+	 * 	- suffix => Any suffixes
+	 * @return object
+	 */
 	function getAuthor($aid) {
 		module_load_include('inc', 'biblio', 'includes/biblio.contributors');
 		$author = biblio_get_contributor($aid);
@@ -36,10 +57,12 @@ class RpcRouter {
 		];
 	}
 
-	function getTaxonRaw($n){
-		return taxonomy_term_load($n);
-	}
-
+	/**
+	 * Gets the ID of the parent node
+	 *
+	 * @param number $tid The ID of the node under question
+	 * @return number The ID of the parent
+	 */
 	function getParent($tid) {
 		$term = taxonomy_term_load($tid);
 		$vid = $term->vid;
@@ -49,6 +72,18 @@ class RpcRouter {
 		return array_keys(taxonomy_get_parents($aantid ?? $tid))[0];
 	}
 
+	/**
+	 * Gets data about a given Taxon
+	 *
+	 * Returns:
+	 * 	- id => The taxon id
+	 * 	- name => The taxon name
+	 * 	- rank => The taxonomic rank
+	 *  - authors => The name(s) of the taxon authors
+	 *  - source => The ID of the source associated with this taxon
+	 * @param number $n Taxon ID
+	 * @return object
+	 */
 	function getTaxon($n) {
 		$term = taxonomy_term_load($n);
 
@@ -65,8 +100,47 @@ class RpcRouter {
 		];
 	}
 
+	/**
+	 * Gets the children of a taxon specified by ID
+	 *
+	 * Returns an array of objects:
+	 * 	- tid => The ID of the child taxon
+	 *  - name => The name of the child taxon
+	 *
+	 * @param number $tid
+	 * @return array
+	 */
+	function getChildren($tid) {
+		return array_values(array_map(
+			function($term) { return [
+				'tid'=>$term->tid,
+				'name'=>$term->name
+			]; },
+			taxonomy_get_children($tid)
+		));
+	}
 
-
+	/**
+	 * Gets the data required to render a "Taxon overview" page
+	 *
+	 * Returns an object with the same return values as getTaxon
+	 * With the additional fields:
+	 * 	- ancestors => An array of all ancestors (tid, name and rank)
+	 * 	- tree => The root node of a tree of related taxa.
+	 *
+	 * Each tree node contains the keys:
+	 * 	- tid => The id of the taxon
+	 * 	- name => The taxon name
+	 *  - children => An array of nodes who are direct descendants
+	 *
+	 * The nodes in the tree are selected based on these criteria:
+	 * 	- All ancestors of the subject taxon
+	 *  - All children of those ancestors
+	 *  - All children of the subject taxon
+	 *
+	 * @param number $n
+	 * @return Object
+	 */
 	function getTaxonOverview($n) {
 		$t = $this->getTaxon($n);
 
@@ -87,7 +161,8 @@ class RpcRouter {
 
 		foreach($ancestors as $term) {
 			$children = array_map(
-				function($term) { return ['tid'=>$term->tid, 'name'=>$term->name]; },
+				function($term) { return [
+					'tid'=>$term->tid,'name'=>$term->name]; },
 				taxonomy_get_children($term->tid)
 			);
 
@@ -105,6 +180,15 @@ class RpcRouter {
 		return $t;
 	}
 
+	/**
+	 * Gets a list of synonyms
+	 *
+	 * Returns array of objects
+	 * (Todo: What's the return structure?)
+	 *
+	 * @param Number Taxon ID $tid
+	 * @return Array
+	 */
 	function getSynonyms($tid) {
 		$vid = taxonomy_term_load($tid)->vid;
 		$field_aan = "field_aan_$vid";
@@ -122,6 +206,21 @@ class RpcRouter {
 		);
 	}
 
+	/**
+	 * Gets a taxon source (biblio node)
+	 *
+	 * Returns:
+	 * 	- type => The Bibtex type of the source
+	 *  - year => The year of the publication
+	 *  - journal => The original publication
+	 *  - title => The source title
+	 *  - language => The source language
+	 *  - contributors => An array of contributor IDs
+	 *  - taxonName => The ID of the related taxon
+	 *
+	 * @param number $n Taxon ID
+	 * @return Object
+	 */
 	function getSource($n) {
 		$source = node_load($n);
 
@@ -144,52 +243,22 @@ class RpcRouter {
 			),
 			'taxonName' => $source->field_taxonomic_name['und'][0]['tid']
 		];
+	}
 
-		// s = Source::Bibtex.create!(
-		// 	bibtex_type: 'article',
-		// 	year: 1838,
-		// 	journal: 'Sylva Tellur. 56 (1838).',
-		// 	title: 'Sylva Tellur. 56 (1838).',
-		// 	authors: [p],
-		// 	keywords: [b]
-		// )
+
+	// Load a raw drupal node (only use for debugging)
+	function getNode($n) {
+		return node_load($n);
+	}
+
+	// Load a raw drupal taxon (only use for debugging)
+	function getTaxonRaw($n){
+		return taxonomy_term_load($n);
 	}
 }
 
 
 /*
-
-
-function scratchpads_tweaks_rdf_settings(){
-  $ns = 'https://www.w3.org/TR/2020/WD-appmanifest-20200715/#dom-webappmanifest-';
-
-  if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    $j = file_get_contents("php://input");
-    $body = json_decode($j, true);
-
-    if(isset($body[$ns.'name'])) {
-      variable_set('site_name', $body[$ns.'name']);
-    }
-    if(isset($body[$ns.'theme_color'])) {
-      $colour = str_replace('#', '', $body[$ns.'theme_color']);
-      variable_set('scratchpads_custom_colour', $colour);
-      variable_set('scratchpads_colour', 'custom');
-      variable_del('scratchpads_colour_css_file');
-    }
-  }
-
-  $colour = variable_get('scratchpads_colour', SCRATCHPADS_DEFAULT_COLOUR);
-  if($colour == 'custom'){
-    $colour = variable_get('scratchpads_custom_colour');
-  }
-
-  $output = [
-    $ns.'name' => variable_get('site_name', 'Scratchpad'),
-    $ns.'theme_color' => '#' . $colour
-  ];
-
-  drupal_json_output($output);
-}
 
 function scratchpads_tweaks_rdf_taxon_concepts(){
   $out = [];
