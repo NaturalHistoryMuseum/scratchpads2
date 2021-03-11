0c58561447b4aa7c75ff322071df86b9b5687c09
Josh Humphries <j.humphries@nhm.ac.uk>
2021-03-11 16:34:48 +0000

    Fix loading of google charts

--- sites/all/modules/custom/simple_google_charts/js/simple_google_charts.js
+++ sites/all/modules/custom/simple_google_charts/js/simple_google_charts.js
@@ -1,15 +1,21 @@
-(function($){
-  google.load('visualization', '1.0', {'packages': ['corechart']});
-  google.setOnLoadCallback(simpleGoogleChartsStatisticsDrawChart);
-  function simpleGoogleChartsStatisticsDrawChart(){
-    $('div.google_chart').once(function(){
-      var chart_id = $(this).attr('id');
-      var data = new google.visualization.arrayToDataTable($.makeArray(Drupal.settings.scratchpads_statistics[chart_id]['data']));
-      var chart = new google.visualization[Drupal.settings.scratchpads_statistics[chart_id]['type']]($('#'+chart_id).get(0));
-      chart.draw(data, Drupal.settings.scratchpads_statistics[chart_id]['options']);
-    });
-  }
-  Drupal.behaviors.simple_google_charts = {attach: function(context, settings){
-    simpleGoogleChartsStatisticsDrawChart();
-  }};
+(function ($) {
+    function simpleGoogleChartsStatisticsDrawChart() {
+        google.load('visualization', '1.0', {'packages': ['corechart']});
+        google.setOnLoadCallback(drawChart);
+
+        function drawChart() {
+            $('div.google_chart').once(function () {
+                var chart_id = $(this).attr('id');
+                var data = new google.visualization.arrayToDataTable($.makeArray(Drupal.settings.scratchpads_statistics[chart_id]['data']));
+                var chart = new google.visualization[Drupal.settings.scratchpads_statistics[chart_id]['type']]($('#' + chart_id).get(0));
+                chart.draw(data, Drupal.settings.scratchpads_statistics[chart_id]['options']);
+            });
+        }
+    }
+
+    Drupal.behaviors.simple_google_charts = {
+        attach: function (context, settings) {
+            simpleGoogleChartsStatisticsDrawChart();
+        }
+    };
 })(jQuery);
