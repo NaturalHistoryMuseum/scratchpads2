580432fde973e79ae20a433ca6f77df589b98f3c
Rob Davis <rob.davis@nhm.ac.uk>
2022-12-01 12:38:38 +0000

    https://github.com/NaturalHistoryMuseum/scratchpads2/issues/6611 removed submodules but kept folders, this means that all the vendor libraries will be controlled under main scratchpads repo

---
+++ sites/all/libraries/vendor/doctrine/collections/tests/Doctrine/Tests/Common/Collections/AbstractLazyArrayCollectionTest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Doctrine\Tests\Common\Collections;
+
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\Tests\LazyArrayCollection;
+
+use function assert;
+
+/**
+ * Tests for {@see \Doctrine\Common\Collections\AbstractLazyCollection}.
+ *
+ * @covers \Doctrine\Common\Collections\AbstractLazyCollection
+ */
+class AbstractLazyArrayCollectionTest extends BaseArrayCollectionTest
+{
+    /**
+     * @param mixed[] $elements
+     *
+     * @return Collection<mixed>
+     */
+    protected function buildCollection(array $elements = []): Collection
+    {
+        return new LazyArrayCollection(new ArrayCollection($elements));
+    }
+
+    public function testLazyCollection(): void
+    {
+        $collection = $this->buildCollection(['a', 'b', 'c']);
+        assert($collection instanceof LazyArrayCollection);
+
+        self::assertFalse($collection->isInitialized());
+        self::assertCount(3, $collection);
+    }
+}
