cf19473e37d8549a69469476736c9f8a33caace3
benscott <ben@benscott.co.uk>
2025-05-29 11:00:01 +0100

    rm haproxy

--- docker/proxysql/files/docker-entrypoint.sh
+++ docker/proxysql/files/docker-entrypoint.sh
@@ -27,33 +27,11 @@ done
 
 echo "ProxySQL admin interface is up. Loading runtime configuration..."
 
-# Execute LOAD commands using mysql client
+# Bug fix - explicitly set username and password
 mysql -h 127.0.0.1 -P 6032 -u admin -p${MYSQL_ROOT_PASSWORD} -e "SET mysql-monitor_username = '${MYSQL_USER}';"
 mysql -h 127.0.0.1 -P 6032 -u admin -p${MYSQL_ROOT_PASSWORD} -e "SET mysql-monitor_password = '${MYSQL_PASSWORD}';"
 mysql -h 127.0.0.1 -P 6032 -u admin -p${MYSQL_ROOT_PASSWORD} -e "LOAD MYSQL VARIABLES TO RUNTIME;"
 mysql -h 127.0.0.1 -P 6032 -u admin -p${MYSQL_ROOT_PASSWORD} -e "SAVE MYSQL VARIABLES TO DISK;"
-
-# Assign MySQL servers to hostgroups:
-#mysql -h 127.0.0.1 -P 6032 -u admin -p${MYSQL_ROOT_PASSWORD} -e "UPDATE mysql_servers SET hostgroup_id = 10 WHERE hostname = 'scratchpads.mysql-master';"
-#mysql -h 127.0.0.1 -P 6032 -u admin -p${MYSQL_ROOT_PASSWORD} -e "UPDATE mysql_servers SET hostgroup_id = 20 WHERE hostname = 'scratchpads.mysql-slave';"
-#mysql -h 127.0.0.1 -P 6032 -u admin -p${MYSQL_ROOT_PASSWORD} -e "INSERT INTO mysql_replication_hostgroups (writer_hostgroup, reader_hostgroup, comment) VALUES (10, 20, 'master/slave failover');"
-#mysql -h 127.0.0.1 -P 6032 -u admin -p${MYSQL_ROOT_PASSWORD} -e "LOAD MYSQL SERVERS TO RUNTIME;"
-#mysql -h 127.0.0.1 -P 6032 -u admin -p${MYSQL_ROOT_PASSWORD} -e "SAVE MYSQL SERVERS TO DISK;"
-
-# Add query rules (only for SELECTs)
-#mysql -h 127.0.0.1 -P 6032 -u admin -p${MYSQL_ROOT_PASSWORD} -e "INSERT INTO mysql_query_rules (rule_id, active, match_pattern, destination_hostgroup, apply) VALUES (1, 1, '.*', 10, 1);"
-#mysql -h 127.0.0.1 -P 6032 -u admin -p${MYSQL_ROOT_PASSWORD} -e "INSERT INTO mysql_query_rules (rule_id, active, match_pattern, destination_hostgroup, apply) VALUES (2, 1, '^SELECT.*', 20, 1);"
-#mysql -h 127.0.0.1 -P 6032 -u admin -p${MYSQL_ROOT_PASSWORD} -e "LOAD MYSQL QUERY RULES TO RUNTIME;"
-#mysql -h 127.0.0.1 -P 6032 -u admin -p${MYSQL_ROOT_PASSWORD} -e "SAVE MYSQL QUERY RULES TO DISK;"
-
-
-# Make sure your user connects via hostgroup_id = 10
-#mysql -h 127.0.0.1 -P 6032 -u admin -p${MYSQL_ROOT_PASSWORD} -e "UPDATE mysql_users SET default_hostgroup = 10 WHERE username = '${MYSQL_USER}';"
-#mysql -h 127.0.0.1 -P 6032 -u admin -p${MYSQL_ROOT_PASSWORD} -e "LOAD MYSQL USERS TO RUNTIME;"
-#mysql -h 127.0.0.1 -P 6032 -u admin -p${MYSQL_ROOT_PASSWORD} -e "SAVE MYSQL USERS TO DISK;"
-
-
-
 
 echo "Runtime configuration loaded."
 
